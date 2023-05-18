9e47d284338d5561d514c05afbcb12dc2ac9db71
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2023-05-18 09:21:07 +0200

    First commit
    Electron with react setup

---
+++ electron/index.js
@@ -0,0 +1,14 @@
+const { app, BrowserWindow } = require('electron')
+
+const createMainWindow = () => {
+    const win = new BrowserWindow({
+      width: 1000,
+      height: 600
+    })
+    
+    win.loadURL('http://localhost:3000');
+}
+
+app.whenReady().then(() => {
+    createMainWindow()
+})
