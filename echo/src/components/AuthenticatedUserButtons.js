d15643e9cb08fa808738f494b05ce1d336ff5908
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2023-05-19 10:37:46 +0200

    Modified the enter room button
    Now there is an option to logout before entering the room.
    Changed the buttons shown to logged in users to reflect this change.

---
+++ echo/src/components/AuthenticatedUserButtons.js
@@ -0,0 +1,56 @@
+import { Button, ButtonGroup } from '@mui/material'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import React from 'react'
+import { useNavigate } from "react-router-dom";
+
+var api = require('../api')
+
+const theme = createTheme({
+    palette: {
+        primary: { main: '#f5e8da', },
+        secondary: { main: '#ce8ca5', },
+    },
+});
+
+const AuthenticatedUserButtons = ({ visibility, nickname}) => {
+    let navigate = useNavigate();
+
+    const logout = async () => {
+        localStorage.removeItem("userId");
+        localStorage.removeItem("userNick");
+
+        window.location.reload();
+
+    }
+
+    const enterRoom = async () => {
+        const res = await api.call('setOnline/' + nickname + '/T');
+        if (res.ok) {
+            navigate("/main");
+        } else {
+            console.error("Could not set user as online");
+        }
+    }
+    if (!visibility) return null
+    return (
+        <ThemeProvider theme={theme}>
+            <ButtonGroup
+                size='large'
+                orientation='vertical'
+                disableElevation
+                variant="text"
+                visibility={!visibility}
+                className="loginButtons"
+            >
+                <Button onClick={enterRoom}>Enter</Button>
+                <Button onClick={logout}>Logout</Button>
+            </ButtonGroup>
+        </ThemeProvider>
+    )
+}
+
+AuthenticatedUserButtons.defaultProps = {
+    visibility: true
+}
+
+export default AuthenticatedUserButtons
--- echo/src/components/Sidebar.js
+++ echo/src/components/Sidebar.js
@@ -6,33 +6,33 @@ var api = require('../api')
 
 const theme = createTheme({
   palette: {
-    primary: {main: '#f5e8da',},
-    secondary: {main: '#ce8ca5',},
+    primary: { main: '#f5e8da', },
+    secondary: { main: '#ce8ca5', },
   },
 });
 
 function Sidebar() {
-  let navigate = useNavigate(); 
+  let navigate = useNavigate();
 
   const logout = async () => {
     //Notify api
     var nickname = localStorage.getItem("userNick");
-    const res = await api.call('setOnline/' + nickname +'/F');
-    if(!res.ok){
+    const res = await api.call('setOnline/' + nickname + '/F');
+    if (!res.ok) {
       console.error("Could not set user as offline");
-    } 
+    }
 
     localStorage.removeItem("userId");
     localStorage.removeItem("userNick");
 
 
     navigate("/");
-    
-}
+
+  }
 
   return (
     <div className='sidebar'>
-      <RoomControl/>
+      <RoomControl />
       <Divider style={{ background: '#f5e8da' }} variant="middle" />
       <Button theme={theme} variant="outlined" onClick={logout}>Logout</Button>
     </div>
