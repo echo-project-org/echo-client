a05b4bf6c98a8b5d8023222e5bd25e1977ca7868
Edo <edoardo.balsamo@sbw-media.ch>
2023-05-25 17:16:24 +0200

    chatControl - styling fixed

--- echo/src/components/ChatControls.js
+++ echo/src/components/ChatControls.js
@@ -2,28 +2,55 @@ import React from 'react'
 import { createTheme } from '@mui/material/styles';
 import TextField from '@mui/material/TextField';
 import { ThemeProvider } from '@emotion/react';
+import { styled } from "@mui/material/styles";
 import MessageBoxButtons from './MessageBoxButtons';
+
+const StyledTextField = styled(TextField)({
+    "& label": {
+        color: "#f5e8da"
+    },
+    "& label.Mui-focused": {
+        color: "#f5e8da"
+    },
+    "& .MuiInput-underline:after": {
+        borderBottomColor: "#f5e8da"
+    },
+    "& .css-15zxrnv-MuiInputBase-root-MuiOutlinedInput-root ": {
+        color: "#f5e8da"
+    },
+    "& .MuiOutlinedInput-root": {
+        "& fieldset": {
+            borderColor: "#f5e8da"
+        },
+        "&:hover fieldset": {
+            borderColor: "#f5e8da"
+        },
+        "&.Mui-focused fieldset": {
+            borderColor: "#f5e8da"
+        }
+    }
+});
 
 function ChatControls() {
     const theme = createTheme({
         palette: {
             primary: { main: '#f5e8da', },
             secondary: { main: '#ce8ca5', },
-        },
+        }
     });
 
     return (
         <div className='chatControls'>
             <ThemeProvider theme={theme}>
                 <form className='chatForm' noValidate autoComplete="off">
-                    <TextField
+                    <StyledTextField
                         sx={{
-                            input:{
-                                color: '#f5e8da',
-                            }}}
+                            width: 750,
+                            input: {color: '#f5e8da'}
+                        }}
                         id="messageBox"
-                        multiline
                         fullWidth
+                        multiline
                         rows={2}
                         label="Send a message."
                         InputProps={{
