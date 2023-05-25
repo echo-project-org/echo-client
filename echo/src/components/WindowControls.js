fb0876570111d008faea7d673488bc750856ea33
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2023-05-25 17:29:01 +0200

    Closing app is now gracefull
    Closing the app will not update the api and stop gracefully the audio streams

--- echo/src/components/WindowControls.js
+++ echo/src/components/WindowControls.js
@@ -4,11 +4,14 @@ import Button from '@mui/material/Button'
 import { ButtonGroup } from '@mui/material'
 import { createTheme } from '@mui/material/styles';
 import { ThemeProvider } from '@emotion/react';
-
 import CloseIcon from '@mui/icons-material/Close';
 import FullscreenIcon from '@mui/icons-material/Fullscreen';
 import MinimizeIcon from '@mui/icons-material/Minimize';
 
+const api = require('../api')
+const ep = require('../echoProtocol')
+const ar = require('../audioReceiver')
+const at = require('../audioTransmitter')
 const { ipcRenderer } = window.require('electron');
 
 const theme = createTheme({
@@ -20,6 +23,11 @@ const theme = createTheme({
 
 function WindowControls({ muted, audioMuted }) {
     const closeApp = async () => {
+        at.stopAudioStream();
+        ar.stopOutputAudioStream();
+        var nickname = localStorage.getItem("userNick");
+        await api.call('setOnline/' + nickname + '/F');
+        
         ipcRenderer.send("exitApplication", true);
     }
 
