fe965bdd0b1fd2e419ff93934c4a4068fc91d56c
zThundy <antonioantonelli7@gmail.com>
2023-05-25 19:18:51 +0200

    server now works

--- echo-server/classes/users.js
+++ echo-server/classes/users.js
@@ -1,59 +1,50 @@
-class Users {
-    constructor(socket) {
+class User {
+    constructor(socket, id) {
+        this.id = id;
         this.socket = socket;
         this.socketId = socket.id;
         console.log("new user, socketid", this.socketId);
 
-        this.users = new Map();
-        this.rooms = new Map();
+        this.remoteUsers = new Map();
 
-        // dev only
-        this.rooms.set(0, { users: this.users });
-
-        this.socket.on("join", (data) => {
-            console.log("new user", data);
-            this.socket.send("ready");
-            this.users.set(data.id, new User(socket));
-            // join user in room
-            this.rooms.set(data.roomId, data.id);
-        });
+        this.socket.on("join", (data) => { this.join(data) });
+        this.socket.on("end", (id) => { this.end(id) });
+        this.socket.on("mute", (id) => { this.mute(id); })
     }
 
-    sendAudioInRoom(roomId = 0, userId = 0, packet) {
-        const room = this.rooms.get(roomId);
-        if (room) {
-            // console.log("this.users", packet);
-            this.users.forEach((user, id) => {
-                if (id !== userId) {
-                    console.log("sending audio to", id)
-                    this.socket.send("receiveAudioPacket", packet);
-                }
-            });
-        }
-    }
-}
-
-class User extends Users {
-    constructor(socket, id) {
-        super(socket);
-        this.id = id;
-        this.socket = socket;
-        this.events();
+    mute(id) {
+        
     }
 
-    events() {
+    end(id) {
+        console.log("requesting end ws with", id, this.socketId);
+        this.socket.disconnect();
+    }
+
+    join(data) {
+        console.log("new user", data);
+
+        this.registerAudioListener();
+    }
+
+    addRemoteUser(remoteUser) {
+        console.log("adding remote user", remoteUser.id, "to", this.id);
+        this.remoteUsers.set(remoteUser.id, remoteUser);
+        this.socket.emit("ready", remoteUser.id);
+    }
+
+    registerAudioListener(cb) {
+        console.log("started audio listener for user", this.id)
         this.socket.on("audioPacket", (packet) => {
-            console.log("got pack", this.socket.id, packet.id);
+            // console.log("got pack", this.socket.id, packet.id);
             // console.log("got pack")
             // console.log(packet);
-
-            this.sendAudioInRoom(undefined, packet.id, packet);
+            this.remoteUsers.forEach((user, id) => {
+                // console.log(id, userId);
+                // console.log("sending audio to", id, "from", this.id);
+                this.socket.emit("receiveAudioPacket", packet);
+            });
         });
-
-        this.socket.on("end", (id) => {
-            this.socket.disconnect();
-            console.log("requesting end ws with", id, this.socketId);
-        })
     }
 }
 
--- echo-server/main.js
+++ echo-server/main.js
@@ -16,9 +16,30 @@ const colors = new Colors();
 const User = require("./classes/users");
 
 console.log(colors.changeColor("red", "Listening for shit :)"))
-io.on('connection', (IOSocket) => {
+io.use((socket, next) => {
+    const request = socket.request;
+    console.log("request param: ", request._query);
+    next();
+})
+
+io.on('connection', (socket) => {
+    const request = socket.request;
+    const id = request._query["id"];
+    
     console.log(colors.changeColor("green", "New socket connection"));
     
-    const user = new User(IOSocket);
-    users.set(user.socketId, user);
+    const newUser = new User(socket, id);
+    users.set(id, newUser);
+
+    console.log(users);
+
+    users.forEach((uReceiver, receiver) => {
+        users.forEach((uTrasmitter, trasmitter) => {
+            // console.log("trying adding new remote user on connection", id, user.id)
+            if (receiver !== trasmitter) {
+                console.log("adding new remote user on connection", receiver, uReceiver.id)
+                uReceiver.addRemoteUser(uReceiver);
+            }
+        })
+    })
 });
