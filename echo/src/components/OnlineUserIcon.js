386e317b8b814b31dc14640039d37c97edec0d06
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2023-05-25 16:22:24 +0200

    Added volume slider to user modal

--- echo/src/components/OnlineUserIcon.js
+++ echo/src/components/OnlineUserIcon.js
@@ -1,6 +1,10 @@
-import { Badge, Avatar, IconButton } from '@mui/material'
+import { Badge, Avatar, Divider } from '@mui/material'
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
+import Stack from '@mui/material/Stack';
+import Slider from '@mui/material/Slider';
+import VolumeDown from '@mui/icons-material/VolumeDown';
+import VolumeUp from '@mui/icons-material/VolumeUp';
 import React from 'react'
 
 const decodeUrl = (url) => {
@@ -10,8 +14,9 @@ const decodeUrl = (url) => {
     return decodeURIComponent(url);
   }
 }
-function OnlineUserIcon({imgUrl, nick, talking}) {
+function OnlineUserIcon({imgUrl, nick, talking, audioVolume}) {
   const [anchorEl, setAnchorEl] = React.useState(null);
+  const [userVolume, setUserVolulme] = React.useState(0.5);
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -19,6 +24,12 @@ function OnlineUserIcon({imgUrl, nick, talking}) {
   const handleClose = () => {
     setAnchorEl(null);
   };
+
+  const handleVolumeChange = (event, newValue) => {
+    //set user volume
+    setUserVolulme(newValue);
+  };
+
 
   return (
     <div>
@@ -43,6 +54,16 @@ function OnlineUserIcon({imgUrl, nick, talking}) {
             'aria-labelledby': 'userIcon',
           }}
         > 
+          <MenuItem>
+            <div style={{width: "100%"}}>
+              <Stack spacing={2} direction="row" alignItems="center">
+                <VolumeDown />
+                <Slider aria-label="Volume" value={userVolume} onChange={handleVolumeChange} />
+                <VolumeUp />
+              </Stack>
+            </div>
+          </MenuItem>
+          <Divider sx={{ my: 0.5 }} />
           <MenuItem onClick={handleClose}>Send private message</MenuItem>
           <MenuItem onClick={handleClose}>Kick</MenuItem>
           <MenuItem onClick={handleClose}>Ban</MenuItem>
@@ -57,6 +78,7 @@ OnlineUserIcon.defaultProps = {
     imgUrl: "https://kurickigabriele2020.altervista.org/Kury.jpg",
     nick: "Kury",
     talking: false,
+    audioVolume: 0.5,
 }
 
 export default OnlineUserIcon
