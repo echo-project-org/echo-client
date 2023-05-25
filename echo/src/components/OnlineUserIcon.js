26e7969979af425d12d8c15b32e1f8d0f5ac2388
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2023-05-25 16:09:41 +0200

    Added menu to user icon click

--- echo/src/components/OnlineUserIcon.js
+++ echo/src/components/OnlineUserIcon.js
@@ -1,4 +1,6 @@
-import { Badge, Avatar } from '@mui/material'
+import { Badge, Avatar, IconButton } from '@mui/material'
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import React from 'react'
 
 const decodeUrl = (url) => {
@@ -9,12 +11,44 @@ const decodeUrl = (url) => {
   }
 }
 function OnlineUserIcon({imgUrl, nick, talking}) {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <div className="onlineUserIcon">
-      <Badge badgeContent={1} variant="dot" anchorOrigin={{vertical: 'bottom', horizontal: 'right',}} showZero={true} invisible={!talking} color={"success"}>
-        <Avatar alt={nick} src={decodeUrl(imgUrl)} sx={{height: '3.5rem', width:'3.5rem'}}/>
-      </Badge>
-      <p className='onlineUserNick'>{nick}</p>
+    <div>
+      <div> 
+        <div className="onlineUserIcon noselect pointer" onClick={handleClick}
+            size="small"
+            aria-controls={open ? 'account-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}>
+          <Badge badgeContent={1} variant="dot" anchorOrigin={{vertical: 'bottom', horizontal: 'right',}} showZero={true} invisible={!talking} color={"success"}>
+            <Avatar alt={nick} src={decodeUrl(imgUrl)} sx={{height: '3.5rem', width:'3.5rem'}}/>
+          </Badge>
+          <p className='onlineUserNick'>{nick}</p>
+        </div>
+
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            'aria-labelledby': 'userIcon',
+          }}
+        > 
+          <MenuItem onClick={handleClose}>Send private message</MenuItem>
+          <MenuItem onClick={handleClose}>Kick</MenuItem>
+          <MenuItem onClick={handleClose}>Ban</MenuItem>
+          <MenuItem onClick={handleClose}>Close menu</MenuItem>
+        </Menu>
+      </div>
     </div>
   )
 }
