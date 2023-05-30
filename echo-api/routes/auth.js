d3d0901981efda36b360d8a0ce3325df19d8e36b
zThundy <antonioantonelli7@gmail.com>
2023-05-30 04:03:24 +0200

    moved auth to custom router

---
+++ echo-api/routes/auth.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const router = express.Router();
+
+router.post("/register", (req, res) => {
+    const { username, password, email } = req.body;
+
+    if (!req.utils.checkEmail(email)) return res.status(406).send({ message: "Invalid email address. (Nice try...)" });
+
+    // this is bad, prepare statements are better
+    req.database.query("INSERT INTO users (name, email, password) VALUES ('" + username + "', '" + email + "', '" + password + "')", (err, result, fields) => {
+        if (err) return res.status(406).send({ message: "Username or email already exists." });
+        
+        if (result && result.affectedRows > 0) {
+            return res.status(200).json({ message: "Account created successfully!" });
+        }
+
+        res.status(406).json({ message: "Username already exists" });
+    });
+});
+
+router.post("/login", (req, res) => {
+    const { email, password } = req.body;
+    
+    if (!req.utils.checkEmail(email)) return res.status(406).send({ message: "Invalid email address. (Nice try...)" });
+
+    req.database.query("SELECT id, name, email FROM users WHERE password = '" + password + "'", (err, result, fields) => {
+        if (err) return res.status(400).send({ message: "You messed up the request." });
+        // send wrong credentials if no user was found
+        if (!result) return res.status(401).send({ message: "Wrong credentials." });
+
+        if (result && result.length > 0) {
+            const { token, refreshToken } = req.authenticator.generateToken(result[0].id);
+            return res.status(200).json({
+                id: result[0].id,
+                name: result[0].name,
+                email: result[0].email,
+                token,
+                refreshToken
+            });
+        }
+
+        res.status(406).json({ message: "Username does not exist or password is incorrect." });
+    });
+});
+
+module.exports = router;
