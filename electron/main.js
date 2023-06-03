28546f27e1dd05d7324ccfbc96630e69f76f5fed
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2023-06-03 18:32:24 +0200

    Added tray icon

--- electron/main.js
+++ electron/main.js
@@ -1,35 +1,62 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
+const { app, BrowserWindow, ipcMain, Tray, Menu } = require('electron')
+const path = require('path')
 
 var mainWindow;
 
 const createMainWindow = () => {
-    var win = new BrowserWindow({
-      width: 1000,
-      height: 600,
-      title: "Echo",
-      frame: false,
-      icon: 'images/echoIcon',
-      webPreferences: {
-        nodeIntegration: true,
-        contextIsolation: false,
-        devTools: true
-      }
-    })
-
-    win.setMinimumSize(800, 500);
-
-    if(app.isPackaged) {
-      win.loadFile('index.html'); // prod
-    }else{
-      win.loadURL('http://localhost:3000'); // dev
+  var win = new BrowserWindow({
+    width: 1000,
+    height: 600,
+    title: "Echo",
+    frame: false,
+    icon: 'images/echoIcon',
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      devTools: true
     }
+  })
 
-    return win;
+  win.setMinimumSize(800, 500);
+
+  if (app.isPackaged) {
+    win.loadFile('index.html'); // prod
+  } else {
+    win.loadURL('http://localhost:3000'); // dev
+  }
+
+  return win;
 }
 
+let tray = null
 
 app.whenReady().then(() => {
   mainWindow = createMainWindow()
+
+  tray = new Tray('images/echoIcon.ico')
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: "Echo",
+      enabled: false
+    },
+    {
+      type: 'separator',
+    },
+    {
+      label: "Show app",
+      click: function () {
+        mainWindow.show();
+      }
+    },
+    {
+      label: "Close app",
+      click: function () {
+        mainWindow.close();
+      }
+    },
+  ])
+  tray.setToolTip('Echo')
+  tray.setContextMenu(contextMenu)
 })
 
 app.on('window-all-closed', () => {
@@ -53,7 +80,7 @@ ipcMain.on("minimize", (event, arg) => {
 })
 
 ipcMain.on("toggleFullscreen", (event, arg) => {
-  if(mainWindow.isMaximized()){
+  if (mainWindow.isMaximized()) {
     mainWindow.unmaximize();
   } else {
     mainWindow.maximize();
