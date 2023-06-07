543e3e95da1ece6dfaa3dc6ea9457cba853cfd26
zThundy <antonioantonelli7@gmail.com>
2023-06-07 13:20:25 +0200

    updated api class to handle errors

--- echo/src/api.js
+++ echo/src/api.js
@@ -26,11 +26,11 @@ export async function call(path, method = "GET", body = null) {
                     )));
             })
             .then(({ status, json, ok }) => {
-                const message = json.message;
-                if (!ok) return reject({ status, message });
+                if (!handleErrors(status, json)) return reject({ status, message: json.message || "Something went wrong :(" });
+                if (!ok) return reject({ status, message: json.message });
                 if (typeof json === "string") json = JSON.parse(json);
                 console.log("response", json)
-                resolve({ status, message, json });
+                resolve({ status, message: json.message, json, ok });
             })
             .catch(reject);
         else
@@ -53,12 +53,48 @@ export async function call(path, method = "GET", body = null) {
                     )));
             })
             .then(({ status, json, ok }) => {
-                const message = json.message;
-                if (!ok) return reject({ status, message });
+                if (!handleErrors(status, json)) return reject({ status, message: json.message || "Something went wrong :(" });
+                if (!ok) return reject({ status, message: json.message });
                 if (typeof json === "string") json = JSON.parse(json);
                 console.log("response", json)
-                resolve({ status, message, json });
+                resolve({ status, message: json.message, json, ok });
             })
             .catch(reject);
     });
+}
+
+const handleErrors = (status, response) => {
+    if (status !== 200) {
+        if (response.message) {
+            console.log(status + ": " + response.message);
+            return false;
+        }
+        else
+            switch (status) {
+                case 400:
+                    // Bad request
+                    console.log(status + ": Bad request");
+                    break;
+                case 401:
+                    // Unauthorized
+                    console.log(status + ": Unauthorized");
+                    break;
+                case 403:
+                    // Forbidden
+                    console.log(status + ": Forbidden");
+                    break;
+                case 404:
+                    // Not found
+                    console.log(status + ": Not found");
+                    break;
+                case 500:
+                    // Internal server error
+                    console.log(status + ": Internal server error");
+                    break;
+                default:
+                    console.log(status + ": Unknown error");
+                    break;
+            }
+    }
+    return response;
 }
