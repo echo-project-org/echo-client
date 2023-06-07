74986a02d59d5eaf64ebba864f451529ced6779e
zThundy <antonioantonelli7@gmail.com>
2023-06-07 18:32:53 +0200

    changed db file

--- echo-api/db.sql
+++ echo-api/db.sql
@@ -1,71 +1,6 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               10.4.28-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win64
--- HeidiSQL Version:             12.5.0.6677
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
 -- Dumping database structure for echo
-CREATE DATABASE IF NOT EXISTS `echo` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+CREATE DATABASE IF NOT EXISTS `echo`;
 USE `echo`;
-
--- Dumping structure for table echo.audiosettings
-CREATE TABLE IF NOT EXISTS `audiosettings` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `sampleRate` int(11) NOT NULL,
-  `bits` int(11) NOT NULL,
-  `channels` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- Data exporting was unselected.
-
--- Dumping structure for table echo.rooms
-CREATE TABLE IF NOT EXISTS `rooms` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(500) NOT NULL DEFAULT 'Unnamed',
-  `description` text NOT NULL,
-  `maxUsers` int(11) NOT NULL DEFAULT 200,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- Data exporting was unselected.
-
--- Dumping structure for table echo.room_users
-CREATE TABLE IF NOT EXISTS `room_users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userId` int(11) DEFAULT NULL,
-  `roomId` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_users_roomusers` (`userId`),
-  KEY `fk_rooms_roomusers` (`roomId`),
-  CONSTRAINT `fk_rooms_roomusers` FOREIGN KEY (`roomId`) REFERENCES `rooms` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION,
-  CONSTRAINT `fk_users_roomusers` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=172 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- Data exporting was unselected.
-
--- Dumping structure for table echo.userfriends
-CREATE TABLE IF NOT EXISTS `userfriends` (
-  `id` int(11) NOT NULL,
-  `otherId` int(11) NOT NULL,
-  KEY `fk_userFriends_users_id` (`id`),
-  KEY `fk_userFriends_users_otherId` (`otherId`),
-  CONSTRAINT `fk_userFriends_users_id` FOREIGN KEY (`id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  CONSTRAINT `fk_userFriends_users_otherId` FOREIGN KEY (`otherId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- Data exporting was unselected.
 
 -- Dumping structure for table echo.users
 CREATE TABLE IF NOT EXISTS `users` (
@@ -83,7 +18,45 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `u_email` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Data exporting was unselected.
+-- Dumping structure for table echo.rooms
+CREATE TABLE IF NOT EXISTS `rooms` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(500) NOT NULL DEFAULT 'Unnamed',
+  `description` text NOT NULL,
+  `maxUsers` int(11) NOT NULL DEFAULT 200,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping structure for table echo.room_users
+CREATE TABLE IF NOT EXISTS `room_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) DEFAULT NULL,
+  `roomId` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_users_roomusers` (`userId`),
+  KEY `fk_rooms_roomusers` (`roomId`),
+  CONSTRAINT `fk_rooms_roomusers` FOREIGN KEY (`roomId`) REFERENCES `rooms` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION,
+  CONSTRAINT `fk_users_roomusers` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=172 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping structure for table echo.audiosettings
+CREATE TABLE IF NOT EXISTS `audiosettings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sampleRate` int(11) NOT NULL,
+  `bits` int(11) NOT NULL,
+  `channels` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping structure for table echo.userfriends
+CREATE TABLE IF NOT EXISTS `userfriends` (
+  `id` int(11) NOT NULL,
+  `otherId` int(11) NOT NULL,
+  KEY `fk_userFriends_users_id` (`id`),
+  KEY `fk_userFriends_users_otherId` (`otherId`),
+  CONSTRAINT `fk_userFriends_users_id` FOREIGN KEY (`id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `fk_userFriends_users_otherId` FOREIGN KEY (`otherId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Dumping structure for table echo.uservolumes
 CREATE TABLE IF NOT EXISTS `uservolumes` (
@@ -94,11 +67,3 @@ CREATE TABLE IF NOT EXISTS `uservolumes` (
   KEY `u_otherId` (`otherId`),
   CONSTRAINT `fk_users_userVolumes` FOREIGN KEY (`id`) REFERENCES `users` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- Data exporting was unselected.
-
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
