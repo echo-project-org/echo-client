038a27f6af4618c24a1af16d01b674da1027a139
zThundy <antonioantonelli7@gmail.com>
2023-06-18 12:33:15 +0200

    added new sql file

--- echo-api/db.sql
+++ echo-api/db.sql
@@ -1,8 +1,6 @@
--- Dumping database structure for echo
 CREATE DATABASE IF NOT EXISTS `echo`;
 USE `echo`;
 
--- Dumping structure for table echo.users
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(300) NOT NULL,
@@ -16,18 +14,30 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `u_name` (`name`),
   UNIQUE KEY `u_email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping structure for table echo.rooms
 CREATE TABLE IF NOT EXISTS `rooms` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(500) NOT NULL DEFAULT 'Unnamed',
   `description` text NOT NULL,
   `maxUsers` int(11) NOT NULL DEFAULT 200,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping structure for table echo.room_users
+CREATE TABLE IF NOT EXISTS `room_messages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `message` text DEFAULT NULL,
+  `userId` int(11) DEFAULT NULL,
+  `roomId` bigint(20) DEFAULT NULL,
+  `date` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `insertDate` timestamp NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  KEY `FK_users_roomMessages` (`userId`),
+  KEY `FK_rooms_roomMessages` (`roomId`),
+  CONSTRAINT `FK_rooms_roomMessages` FOREIGN KEY (`roomId`) REFERENCES `rooms` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `FK_users_roomMessages` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 CREATE TABLE IF NOT EXISTS `room_users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `userId` int(11) DEFAULT NULL,
@@ -37,31 +47,20 @@ CREATE TABLE IF NOT EXISTS `room_users` (
   KEY `fk_rooms_roomusers` (`roomId`),
   CONSTRAINT `fk_rooms_roomusers` FOREIGN KEY (`roomId`) REFERENCES `rooms` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION,
   CONSTRAINT `fk_users_roomusers` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=172 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=347 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping structure for table echo.audiosettings
-CREATE TABLE IF NOT EXISTS `audiosettings` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `sampleRate` int(11) NOT NULL,
-  `bits` int(11) NOT NULL,
-  `channels` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- Dumping structure for table echo.userfriends
-CREATE TABLE IF NOT EXISTS `userfriends` (
-  `id` int(11) NOT NULL,
-  `otherId` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `user_friends` (
+  `id` int(11) DEFAULT NULL,
+  `otherId` int(11) DEFAULT NULL,
   KEY `fk_userFriends_users_id` (`id`),
   KEY `fk_userFriends_users_otherId` (`otherId`),
   CONSTRAINT `fk_userFriends_users_id` FOREIGN KEY (`id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
   CONSTRAINT `fk_userFriends_users_otherId` FOREIGN KEY (`otherId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping structure for table echo.uservolumes
-CREATE TABLE IF NOT EXISTS `uservolumes` (
-  `id` int(11) NOT NULL,
-  `otherId` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `user_volumes` (
+  `id` int(11) DEFAULT NULL,
+  `otherId` int(11) DEFAULT NULL,
   `volume` float NOT NULL DEFAULT 1,
   KEY `u_id` (`id`),
   KEY `u_otherId` (`otherId`),
