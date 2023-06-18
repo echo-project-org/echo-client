cf6fec15173de173b9162bd4adffe234a80be076
zThundy <antonioantonelli7@gmail.com>
2023-06-18 19:13:30 +0200

    moved urls to env variables

--- echo/src/api.js
+++ echo/src/api.js
@@ -1,85 +1,85 @@
 export async function call(path, method = "GET", body = null) {
-    return new Promise((resolve, reject) => {
-        // if method is not GET, then add the body to the request
-        if (method !== "GET" && body === null) reject("Body is null");
-        if (method === "GET" && body !== null) reject("Body is not null");
+  return new Promise((resolve, reject) => {
+    // if method is not GET, then add the body to the request
+    if (method !== "GET" && body === null) reject("Body is null");
+    if (method === "GET" && body !== null) reject("Body is not null");
 
-        var options;
+    var options;
 
-        if (method === "GET")
-            options = {
-                method: method,
-                cache: 'no-cache',
-                headers: {
-                    'Content-Type': 'application/json',
-                    "Authorization": "Bearer " + localStorage.getItem("token")
-                }
+    if (method === "GET")
+      options = {
+        method: method,
+        cache: 'no-cache',
+        headers: {
+          'Content-Type': 'application/json',
+          "Authorization": "Bearer " + localStorage.getItem("token")
+        }
+      }
+    else
+      options = {
+        method: method,
+        cache: 'no-cache',
+        headers: {
+          'Content-Type': 'application/json',
+          "Authorization": "Bearer " + localStorage.getItem("token")
+        },
+        body: typeof body === "string" ? body : JSON.stringify(body)
+      }
+
+
+    fetch(process.env.API_URL + path, options)
+      .then((response) => {
+        return new Promise((resolve) => response.json()
+          .then((json) => resolve(
+            {
+              status: response.status,
+              ok: response.ok,
+              json,
             }
-        else
-            options = {
-                method: method,
-                cache: 'no-cache',
-                headers: { 
-                    'Content-Type': 'application/json',
-                    "Authorization": "Bearer " + localStorage.getItem("token")
-                },
-                body: typeof body === "string" ? body : JSON.stringify(body)
-            }
-            
-        
-        fetch('http://kury.ddns.net:6980/' + path, options)
-            .then((response) => {
-                return new Promise((resolve) => response.json()
-                    .then((json) => resolve(
-                        {
-                            status: response.status,
-                            ok: response.ok,
-                            json,
-                        }
-                    )));
-            })
-            .then(({ status, json, ok }) => {
-                if (!handleErrors(status, json)) return reject({ status, message: json.message || "Something went wrong :(" });
-                if (!ok) return reject({ status, message: json.message });
-                if (typeof json === "string") json = JSON.parse(json);
-                resolve({ status, message: json.message, json, ok });
-            })
-            .catch(reject);
-    });
+          )));
+      })
+      .then(({ status, json, ok }) => {
+        if (!handleErrors(status, json)) return reject({ status, message: json.message || "Something went wrong :(" });
+        if (!ok) return reject({ status, message: json.message });
+        if (typeof json === "string") json = JSON.parse(json);
+        resolve({ status, message: json.message, json, ok });
+      })
+      .catch(reject);
+  });
 }
 
 const handleErrors = (status, response) => {
-    if (status !== 200) {
-        if (response.message) {
-            console.log(status + ": " + response.message);
-            return false;
-        }
-        else
-            switch (status) {
-                case 400:
-                    // Bad request
-                    console.log(status + ": Bad request");
-                    break;
-                case 401:
-                    // Unauthorized
-                    console.log(status + ": Unauthorized");
-                    break;
-                case 403:
-                    // Forbidden
-                    console.log(status + ": Forbidden");
-                    break;
-                case 404:
-                    // Not found
-                    console.log(status + ": Not found");
-                    break;
-                case 500:
-                    // Internal server error
-                    console.log(status + ": Internal server error");
-                    break;
-                default:
-                    console.log(status + ": Unknown error");
-                    break;
-            }
+  if (status !== 200) {
+    if (response.message) {
+      console.log(status + ": " + response.message);
+      return false;
     }
-    return response;
+    else
+      switch (status) {
+        case 400:
+          // Bad request
+          console.log(status + ": Bad request");
+          break;
+        case 401:
+          // Unauthorized
+          console.log(status + ": Unauthorized");
+          break;
+        case 403:
+          // Forbidden
+          console.log(status + ": Forbidden");
+          break;
+        case 404:
+          // Not found
+          console.log(status + ": Not found");
+          break;
+        case 500:
+          // Internal server error
+          console.log(status + ": Internal server error");
+          break;
+        default:
+          console.log(status + ": Unknown error");
+          break;
+      }
+  }
+  return response;
 }
--- echo/src/audioRtcReceiver.js
+++ echo/src/audioRtcReceiver.js
@@ -1,195 +1,187 @@
 const sdpTransform = require('sdp-transform');
-const stunkServer = [
-    {
-        username: 'echo',
-        credential: 'echo123',
-        urls: ["turn:kury.ddns.net:6984"]
-    }
-];
-const signalServer = "http://kury.ddns.net:6983";
 const goodOpusSettings = "minptime=10;useinbandfec=1;maxplaybackrate=48000;stereo=1;maxaveragebitrate=510000";
 
 class audioRtcReceiver {
-    constructor(id, senderId, deviceId = 'default', volume = 1.0) {
-        this.id = id;
-        this.senderId = senderId;
-        this.deviceId = deviceId;
-        this.volume = volume;
-        this.peer = null;
-        this.stream = null;
-        this.isReceiving = false;
-        this.isMuted = false;
-        this.audioElement = new Audio();
-        this.context = null;
-        this.gainNode = null;
-        this.personalGainNode = null;
-        this.muteNode = null;
+  constructor(id, senderId, deviceId = 'default', volume = 1.0) {
+    this.id = id;
+    this.senderId = senderId;
+    this.deviceId = deviceId;
+    this.volume = volume;
+    this.peer = null;
+    this.stream = null;
+    this.isReceiving = false;
+    this.isMuted = false;
+    this.audioElement = new Audio();
+    this.context = null;
+    this.gainNode = null;
+    this.personalGainNode = null;
+    this.muteNode = null;
 
 
-        console.log("Created receiver", this.id, this.senderId, this.deviceId, this.volume);
+    console.log("Created receiver", this.id, this.senderId, this.deviceId, this.volume);
+  }
+
+  async init() {
+    this.peer = this.createPeer();
+    this.peer.addTransceiver('audio', { direction: 'recvonly' });
+  }
+
+  setVolume(volume) {
+    if (volume > 1.0 || volume < 0.0) {
+      console.error("Volume must be between 0.0 and 1.0", volume);
+      volume = 1.0;
     }
 
-    async init() {
-        this.peer = this.createPeer();
-        this.peer.addTransceiver('audio', { direction: 'recvonly' });
+    this.volume = volume;
+    console.log(this.audioElement, volume) //This is null, wtf
+    this.personalGainNode.gain.value = volume;
+    this.unmute();
+
+    this.muted = false;
+  }
+
+  setGlobalVolume(volume) {
+    if (volume > 1.0 || volume < 0.0) {
+      console.error("Volume must be between 0.0 and 1.0", volume);
+      volume = 1.0;
     }
 
-    setVolume(volume){
-        if(volume > 1.0 || volume < 0.0){
-            console.error("Volume must be between 0.0 and 1.0", volume);
-            volume = 1.0;
-        }
+    this.gainNode.gain.value = volume;
+  }
 
-        this.volume = volume;
-        console.log(this.audioElement, volume) //This is null, wtf
-        this.personalGainNode.gain.value = volume;
-        this.unmute();
+  setDevice(deviceId) {
+    console.log("Setting device", deviceId);
+    if (deviceId === 'default') {
+      return
+    }
+    this.deviceId = deviceId;
+    this.context.setSinkId(deviceId);
+  }
 
-        this.muted = false;
+  mute() {
+    this.muteNode.gain.value = 0.0;
+    this.isMuted = true;
+  }
+
+  unmute() {
+    this.muteNode.gain.value = 1.0;
+    this.isMuted = false;
+  }
+
+  close() {
+    if (this.audioElement) {
+      this.audioElement.pause();
+      this.audioElement = null;
+    } else {
+      console.log("Elemtn null")
+    }
+    if (this.stream && this.stream.getTracks()) {
+      this.stream.getTracks().forEach(track => track.stop());
+    }
+    else {
+      console.log("stream null")
+    }
+    if (this.peer) {
+      this.peer.close();
+    } else {
+      console.log("peer null")
     }
 
-    setGlobalVolume(volume){
-        if(volume > 1.0 || volume < 0.0){
-            console.error("Volume must be between 0.0 and 1.0", volume);
-            volume = 1.0;
-        }
+    this.stream = null;
+    this.peer = null;
 
-        this.gainNode.gain.value = volume;
+    this.isReceiving = false;
+  }
+
+  createPeer() {
+    const peer = new RTCPeerConnection({
+      iceServers: process.env.ICE_SERVERS
+    });
+    peer.ontrack = (e) => { this.handleTrackEvent(e) };
+    //Handle the ice candidates
+    peer.onnegotiationneeded = () => this.handleNegotiationNeededEvent(peer);
+
+    return peer;
+  }
+
+  handleTrackEvent(e) {
+    this.context = new AudioContext();
+    console.log(this.deviceId)
+    if (this.deviceId !== 'default' && this.deviceId) {
+      this.context.setSinkId(this.deviceId);
     }
+    let source = this.context.createMediaStreamSource(e.streams[0]);
+    let destination = this.context.destination;
 
-    setDevice(deviceId){
-        console.log("Setting device", deviceId);
-        if(deviceId === 'default'){
-            return
-        }
-        this.deviceId = deviceId;
-        this.context.setSinkId(deviceId);
+    this.gainNode = this.context.createGain();
+    this.personalGainNode = this.context.createGain();
+    this.muteNode = this.context.createGain();
+
+    source.connect(this.gainNode);
+    this.gainNode.connect(this.personalGainNode);
+    this.personalGainNode.connect(this.muteNode);
+    this.muteNode.connect(destination);
+
+    this.context.resume();
+
+    //Need this cuz of bug in chrome
+    console.log("Got track event", e)
+    this.stream = e.streams[0];
+    this.audioElement.srcObject = this.stream;
+    this.audioElement.autoplay = true;
+    if (this.deviceId !== 'default') {
+      //this.audioElement.setSinkId(this.deviceId);
     }
+    this.audioElement.pause();
+    this.isReceiving = true;
+  };
 
-    mute() {
-        this.muteNode.gain.value = 0.0;
-        this.isMuted = true;
-    }
+  async handleNegotiationNeededEvent(peer) {
+    const offer = await peer.createOffer();
 
-    unmute() {
-        this.muteNode.gain.value = 1.0;
-        this.isMuted = false;
-    }
+    let parsed = sdpTransform.parse(offer.sdp);
+    parsed.media[0].fmtp[0].config = goodOpusSettings;
+    console.log(parsed)
+    offer.sdp = sdpTransform.write(parsed);
 
-    close() {
-        if(this.audioElement){
-            this.audioElement.pause();
-            this.audioElement = null;
-        } else {
-            console.log("Elemtn null")
-        }
-        if(this.stream && this.stream.getTracks()){
-            this.stream.getTracks().forEach(track => track.stop());
-        }
-        else {
-            console.log("stream null")
-        }
-        if(this.peer){
-            this.peer.close();
-        }else {
-            console.log("peer null")
-        }
-
-        this.stream = null;
-        this.peer = null;
-
-        this.isReceiving = false;
-    }
-
-    createPeer() {
-        const peer = new RTCPeerConnection({
-            iceServers: stunkServer
-        });
-        peer.ontrack = (e) => {this.handleTrackEvent(e)};
-        //Handle the ice candidates
-        peer.onnegotiationneeded = () => this.handleNegotiationNeededEvent(peer);
-
-        return peer;
-    }
-
-    handleTrackEvent(e) {
-        this.context = new AudioContext();
-        console.log(this.deviceId)
-        if(this.deviceId !== 'default' && this.deviceId){
-            this.context.setSinkId(this.deviceId);
-        }
-        let source = this.context.createMediaStreamSource(e.streams[0]);
-        let destination = this.context.destination;
-
-        this.gainNode = this.context.createGain();
-        this.personalGainNode = this.context.createGain();
-        this.muteNode = this.context.createGain();
-
-        source.connect(this.gainNode);
-        this.gainNode.connect(this.personalGainNode);
-        this.personalGainNode.connect(this.muteNode);
-        this.muteNode.connect(destination);
-
-        this.context.resume();
-
-        //Need this cuz of bug in chrome
-        console.log("Got track event", e)
-        this.stream = e.streams[0];
-        this.audioElement.srcObject = this.stream;
-        this.audioElement.autoplay = true;
-        if(this.deviceId !== 'default'){
-            //this.audioElement.setSinkId(this.deviceId);
-        }
-        this.audioElement.pause();
-        this.isReceiving = true;
+    await peer.setLocalDescription(offer);
+    const body = {
+      sdp: peer.localDescription,
+      senderId: this.senderId,
+      receiverId: this.id,
     };
 
-    async handleNegotiationNeededEvent(peer) {
-        const offer = await peer.createOffer();
+    fetch(process.env.SIGNAL_SERVER + 'subscribeAudio', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    }).then((response) => {
+      response.json().then((json) => {
+        const desc = new RTCSessionDescription(json.sdp);
+        peer.setRemoteDescription(desc).catch(e => console.log(e));
+      })
+    });
+  }
 
-        let parsed = sdpTransform.parse(offer.sdp);
-        parsed.media[0].fmtp[0].config = goodOpusSettings;
-        console.log(parsed)
-        offer.sdp = sdpTransform.write(parsed);
-
-        await peer.setLocalDescription(offer);
-        const body = {
-            sdp: peer.localDescription,
-            senderId: this.senderId,
-            receiverId: this.id,
-        };
-
-        fetch(signalServer + '/subscribeAudio', {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(body),
-        }).then((response) => {
-            response.json().then((json) => {
-                const desc = new RTCSessionDescription(json.sdp);
-                peer.setRemoteDescription(desc).catch(e => console.log(e));
+  static async getAudioDevices() {
+    return new Promise((resolve, reject) => {
+      var out = [];
+      navigator.mediaDevices.enumerateDevices().then((devices) => {
+        devices.forEach((device, id) => {
+          if (device.kind === "audiooutput" && device.deviceId !== "communications" && device.deviceId !== "default") {
+            out.push({
+              "name": device.label,
+              "id": device.deviceId
             })
-        });
-    }
-
-    static async getAudioDevices() {
-        return new Promise((resolve, reject) => {
-            var out = [];
-            navigator.mediaDevices.enumerateDevices().then((devices) => {
-                devices.forEach((device, id) => {
-                    if (device.kind === "audiooutput" && device.deviceId !== "communications" && device.deviceId !== "default") {
-                        out.push({
-                            "name": device.label,
-                            "id": device.deviceId
-                        })
-                    }
-                })
-    
-                resolve(out);
-            })
+          }
         })
-    }
+
+        resolve(out);
+      })
+    })
+  }
 
 }
 
