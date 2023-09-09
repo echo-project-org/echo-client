22ab0440b902fcbe504f6d27047e73469c9bd15d
zThundy <antonioantonelli7@gmail.com>
2023-09-09 12:53:46 +0200

    maybe fixed multiroom switch

--- echo/src/components/rooms/Rooms.js
+++ echo/src/components/rooms/Rooms.js
@@ -17,6 +17,7 @@ function Rooms({ setState, connected, updateCurrentRoom }) {
   ])
 
   const onRoomClick = (joiningId) => {
+    if (activeRoomId !== 0) ep.exitFromRoom(localStorage.getItem("id"));
     ep.joinRoom(localStorage.getItem("id"), joiningId);
     api.call("rooms/join", "POST", { userId: localStorage.getItem("id"), roomId: joiningId })
       .then((res) => {
