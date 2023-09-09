271995b8809b0d331c0b9480e594679cca589ce4
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2023-09-09 18:13:46 +0200

    Implemented more functions for videortc

--- echo/src/videoRtc.js
+++ echo/src/videoRtc.js
@@ -84,6 +84,116 @@ class videoRtc {
         return peer;
     }
 
+    async subscribeToVideo(id) {
+        ep.subscribeVideo({
+            senderId: id,
+            receiverId: this.id
+        }, (a) => {
+            if (a) {
+                this.streamIds.set(id, a);
+            } else {
+                console.error("Failed to subscribe to video from", id);
+                return;
+            }
+        })
+    }
+
+    unsubscribeFromVideo(id = null) {
+        if (id) {
+            let streamId = this.streamIds.get(id);
+            if (streamId) {
+                this.inputstreams.filter((stream) => {
+                    return stream.stream.id === streamId;
+                }).forEach((stream) => {
+                    stream.stream.getTracks().forEach((track) => {
+                        track.stop();
+                    });
+                    stream.stream = null;
+                });
+            }
+
+            ep.unsubscribeVideo({ senderId: id, receiverId: this.id });
+        } else {
+            this.inputstreams.forEach((stream) => {
+                stream.stream.getTracks().forEach((track) => {
+                    track.stop();
+                });
+                stream.stream = null;
+            });
+
+            this.inputstreams = [];
+
+            for (const [key, value] of this.streamIds) {
+                console.log(key, "Unsubscribing from", this.id);
+                ep.unsubscribeVideo({ senderId: key, receiverId: this.id });
+            }
+        }
+    }
+
+    async handleNegotiationNeededEvent(peer) {
+        const offer = await peer.createOffer();
+        let parsed = sdpTransform.parse(offer.sdp);
+        //edit sdp to make video look better
+        offer.sdp = sdpTransform.write(parsed);
+
+        await peer.setLocalDescription(offer);
+
+        ep.broadcastVideo({
+            sdp: peer.localDescription,
+            id: this.id
+        }, (description) => {
+            const desc = new RTCSessionDescription(description);
+            peer.setRemoteDescription(desc).catch((e) => console.error(e));
+        })
+    }
+
+    async renegotiate(remoteOffer, cb) {
+        const remoteDesc = new RTCSessionDescription(remoteOffer);
+        this.peer.setRemoteDescription(remoteDesc).then(() => {
+            this.peer.createAnswer().then((answer) => {
+                let parsed = sdpTransform.parse(answer.sdp);
+                //edit sdp to make video look better
+                answer.sdp = sdpTransform.write(parsed);
+
+                this.peer.setLocalDescription(answer).then(() => {
+                    cb(this.peer.localDescription);
+                });
+            });
+        });
+    }
+
+    handleTrackEvent(e) {
+        console.log("Got track event", e);
+        if (e.track.kind === "video") {
+            console.log("Got video track");
+            //Play video stream
+
+            this.inputstreams.push({
+                stream: e.streams[0],
+            });
+        }
+    }
+
+    close() {
+        if (this.stream) {
+            this.stream.getTracks().forEach((track) => {
+                track.stop();
+            });
+            this.stream = null
+        } else {
+            console.log("Stream already closed");
+        }
+
+        if (this.peer) {
+            this.peer.close();
+            this.peer = null;
+        } else {
+            console.log("Peer already closed");
+        }
+
+        ep.stopVideoBroadcast({ id: this.id });
+    }
+
     static async getVideoSources() {
         const srcs = await ipcRenderer.invoke("getVideoSources");
         srcs.forEach((src) => {
