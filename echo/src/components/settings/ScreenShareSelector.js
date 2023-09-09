f95e6a3625f55e4ce50eebc55114c61e6dc66575
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2023-09-09 18:28:36 +0200

    aah pinat batter bebi

---
+++ echo/src/components/settings/ScreenShareSelector.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+function ScreenShareSelector() {
+    const [videoDevices, setVideoDevices] = useState([]);
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default ScreenShareSelector
