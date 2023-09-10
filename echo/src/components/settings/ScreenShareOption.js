0e297b64ad074b9d6cb147341d25b77ce24dadd3
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2023-09-10 09:34:01 +0200

    Added icons to screen share options if available

--- echo/src/components/settings/ScreenShareOption.js
+++ echo/src/components/settings/ScreenShareOption.js
@@ -3,6 +3,9 @@ import React from 'react'
 function ScreenShareOption({ device, clickHandler }) {
   return (
     <div className='screenShareOption' onClick={() => { clickHandler(device.id) }}>
+      {
+        device.appIcon ? <img src={device.appIcon.toDataURL()} alt={device.name} className='noselect screenShareAppIcon' /> : <></>
+      }
       <img src={device.thumbnail.toDataURL()} alt={device.name} className='noselect screenShareThumbnail' />
       <p className='noselect'>
         {device.name}
