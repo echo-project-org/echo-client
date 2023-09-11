f9ed2a12114b9832e4203f76e692b660ec777101
zThundy <antonioantonelli7@gmail.com>
2023-09-11 11:48:56 +0200

    prepare rooms code for cache

--- echo/src/components/rooms/ActiveRoom.js
+++ echo/src/components/rooms/ActiveRoom.js
@@ -1,20 +1,24 @@
 import OnlineUserIcon from '../user/OnlineUserIcon';
 
+import { useEffect } from 'react';
+
+import { ep } from "../../index";
+
 function ActiveRoom({ users, onClick, data }) {
   const handleClick = () => {
     onClick(data.id);
   }
-  
+
   return (
     <div className='room' onClick={handleClick}>
-        <p className='roomName noselect'>{data.name}</p>
-        <div className="roomUsers">
-            {
-              users.map(user => (
-                <OnlineUserIcon key={user.name} imgUrl={user.img} name={user.name} id={user.id} />
-              ))
-            }
-        </div>
+      <p className='roomName noselect'>{data.name}</p>
+      <div className="roomUsers">
+        {
+          users.map(user => (
+            <OnlineUserIcon key={user.name} imgUrl={user.userImage} name={user.name} id={user.id} />
+          ))
+        }
+      </div>
     </div>
   )
 }
--- echo/src/components/rooms/InactiveRoom.js
+++ echo/src/components/rooms/InactiveRoom.js
@@ -3,28 +3,28 @@ import { Avatar, AvatarGroup } from '@mui/material'
 import { styled } from "@mui/material/styles";
 
 const StyledAvatarGroup = styled(AvatarGroup)({
-    "& .MuiAvatar-root": {
-        border: "1px solid #f5e8da"
-    },
+  "& .MuiAvatar-root": {
+    border: "1px solid #f5e8da"
+  },
 });
 
 function InactiveRoom({ users, onClick, data }) {
-    const handleClick = () => {
-        onClick(data.id);
-    }
+  const handleClick = () => {
+    onClick(data.id);
+  }
 
-    return (
-        <div className='secondRoom' onClick={handleClick}>
-            <p className='roomName noselect'>{data.name}</p>
-            <StyledAvatarGroup max={4} sx={{ justifyContent: 'flex-end', marginLeft: '.5rem', marginBottom: '.25rem' }}>
-                {
-                    users.map((user) => (
-                        <Avatar key={user.id} alt={users.name} src={user.img} sx={{ height: '1.25rem', width: '1.25rem', marginBottom: '.5rem' }} />
-                    ))
-                }
-            </StyledAvatarGroup>
-        </div>
-    )
+  return (
+    <div className='secondRoom' onClick={handleClick}>
+      <p className='roomName noselect'>{data.name}</p>
+      <StyledAvatarGroup max={4} sx={{ justifyContent: 'flex-end', marginLeft: '.5rem', marginBottom: '.25rem' }}>
+        {
+          users.map((user) => (
+            <Avatar key={user.id} alt={users.name} src={user.userImage} sx={{ height: '1.25rem', width: '1.25rem', marginBottom: '.5rem' }} />
+          ))
+        }
+      </StyledAvatarGroup>
+    </div>
+  )
 }
 
 export default InactiveRoom
