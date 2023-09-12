3cb0354e62c18499628621b1cbf06300f16a9beb
zThundy <antonioantonelli7@gmail.com>
2023-09-12 17:56:33 +0200

    maybe finished restyle and fix of chat

--- echo/src/components/chat/MessageRight.js
+++ echo/src/components/chat/MessageRight.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import Avatar from '@mui/material/Avatar';
+import { Typography, Grid } from '@mui/material';
 
 function MessageRight({ message }) {
   return (
-    <div className='rightMessage'>
-      <Avatar alt={message.name} src={message.img} sx={{ width: "1.5rem", height: "1.5rem" }} />
-      <div className="rightMessageText">
-        {message.message}
-      </div>
-    </div>
+    <Grid container className='rightMessage' direction={"row"} sx={{ flexFlow: "row" }}>
+      <Grid item>
+        <Avatar alt={message.name} src={message.img} sx={{ width: "1.5rem", height: "1.5rem" }} />
+      </Grid>
+      <Grid item>
+        <Grid container direction={"row"} sx={{ flexFlow: "column" }}>
+          <Grid item>
+            <Typography variant="body1" sx={{ fontWeight: "bold", fontSize: "1rem", textAlign: "left", color: "rgb(115, 24, 115)" }}>
+              {message.name}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <div className="messageText">
+              {message.message}
+            </div>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   )
 }
 
--- echo/src/components/rooms/RoomContent.js
+++ echo/src/components/rooms/RoomContent.js
@@ -1,11 +1,50 @@
 import "../../css/chat.css";
 
 import { useState, useEffect } from 'react'
+import { Container, styled } from '@mui/material'
+
 import Chat from '../chat/Chat'
 import ChatControls from '../chat/ChatControls'
 import EmojiPicker from "../chat/EmojiPicker";
 
 import { ep } from "../../index";
+
+const StyledContainer = styled(Container)(({ theme }) => ({
+  [theme.breakpoints.up('xs')]: {
+    margin: "0 0 0 1rem",
+    height: "calc(100vh - 2rem)",
+    width: "100%",
+    position: "relative",
+    padding: "0",
+    display: "inline-flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    maxWidth: "calc(100vw - 20rem)"
+  },
+  [theme.breakpoints.up('lg')]: {
+    margin: "0 0 0 1rem",
+    height: "calc(100vh - 2rem)",
+    width: "100%",
+    position: "relative",
+    padding: "0",
+    display: "inline-flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    maxWidth: "calc(100vw - 20rem)"
+  },
+  [theme.breakpoints.up('xl')]: {
+    margin: "0 0 0 1rem",
+    height: "calc(100vh - 2rem)",
+    width: "100%",
+    position: "relative",
+    padding: "0",
+    display: "inline-flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    maxWidth: "calc(100vw - 20rem)"
+  },
+}));
+
 
 function RoomContent({ roomId }) {
   const [emojiPicker, setEmojiPicker] = useState(false);
@@ -20,11 +59,9 @@ function RoomContent({ roomId }) {
   });
 
   return (
-    <div className='roomContent'>
-      {
-
-      }
-      
+    <StyledContainer>
+      <Chat currentRoomId={roomId}/>
+      <ChatControls onEmojiOn={handleEmojiPicker} />
       <EmojiPicker show={emojiPicker} style={{
           position: "absolute",
           bottom: "5rem",
@@ -32,10 +69,7 @@ function RoomContent({ roomId }) {
           zIndex: "1000",
         }}
       />
-
-      <Chat currentRoomId={roomId}/>
-      <ChatControls onEmojiOn={handleEmojiPicker}/>
-    </div>
+    </StyledContainer>
   )
 }
 
