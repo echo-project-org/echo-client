4b8347e8994c764a8f70747c2d9ffa0e55e78ee1
zThundy <antonioantonelli7@gmail.com>
2023-09-14 11:47:20 +0200

    added timestamp on right message

--- echo/src/components/chat/MessageRight.js
+++ echo/src/components/chat/MessageRight.js
@@ -19,6 +19,11 @@ function MessageRight({ message }) {
               {message.message}
             </div>
           </Grid>
+          <Grid item>
+            <div className="messageDate">
+              {message.dateDisplay}
+            </div>
+          </Grid>
         </Grid>
       </Grid>
     </Grid>
