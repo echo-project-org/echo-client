fd6ebf267846a34a3ca0e627fc01cadeba97395d
zThundy <antonioantonelli7@gmail.com>
2023-09-16 21:55:54 +0200

    room header done

--- echo/src/components/rooms/RoomContent.js
+++ echo/src/components/rooms/RoomContent.js
@@ -1,24 +1,90 @@
 import "../../css/chat.css";
 
 import { useState, useEffect } from 'react'
+import { Grid, Container, styled } from '@mui/material';
+import { ChatBubble, PeopleAlt } from '@mui/icons-material';
+
 import RoomContentChat from "./RoomContentChat";
 import RoomContentScreenShares from "./RoomContentScreenShares";
 
-
-
+const StyledContainer = styled(Container)(({ theme }) => ({
+  [theme.breakpoints.up('xs')]: {
+    margin: "0 0 0 1rem",
+    height: "calc(12vh - 5rem)",
+    width: "100%",
+    position: "relative",
+    display: "inline-flex",
+    maxWidth: "calc(100vw - 20rem)",
+    backgroundColor: "#3e2542"
+  },
+  [theme.breakpoints.up('lg')]: {
+    margin: "0 0 0 1rem",
+    height: "calc(12vh - 5rem)",
+    width: "100%",
+    position: "relative",
+    display: "inline-flex",
+    maxWidth: "calc(100vw - 20rem)",
+    backgroundColor: "#3e2542"
+  },
+  [theme.breakpoints.up('xl')]: {
+    margin: "0 0 0 1rem",
+    height: "calc(12vh - 5rem)",
+    width: "100%",
+    position: "relative",
+    display: "inline-flex",
+    maxWidth: "calc(100vw - 20rem)",
+    backgroundColor: "#3e2542"
+  },
+}));
 
 function RoomContent({ roomId }) {
   const [hasUsersStreaming, setHasUsersStreaming] = useState(false);
-  const [contentSelected, setContentSelected] = useState("screen");
+  const [contentSelected, setContentSelected] = useState("chat");
 
-  switch (contentSelected) {
-    case "chat":
-      return <RoomContentChat roomId={roomId} />
-    case "screen":
-      return <RoomContentScreenShares roomId={roomId} />
-    default:
-      return <RoomContentChat roomId={roomId} />
+  const computeRoomContent = () => {
+    switch (contentSelected) {
+      case "chat":
+        return <RoomContentChat roomId={roomId} />
+      case "screen":
+        return <RoomContentScreenShares roomId={roomId} />
+      default:
+        return <RoomContentChat roomId={roomId} />
+    }
   }
+
+  return (
+    <Grid container>
+      <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+        <StyledContainer>
+          <Grid container>
+            <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
+              sono il titolo
+              e io la descrizione della stanza
+            </Grid>
+            <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
+              <Container className="iconsContainer">
+                {
+                  contentSelected === "chat" ?
+                    <ChatBubble onClick={() => setContentSelected("chat")} sx={{ color: "#c895ff" }} />
+                    :
+                    <ChatBubble onClick={() => setContentSelected("chat")} />
+                }
+                {
+                  contentSelected === "screen" ?
+                    <PeopleAlt onClick={() => setContentSelected("screen")} sx={{ color: "#c895ff" }} />
+                    :
+                    <PeopleAlt onClick={() => setContentSelected("screen")} />
+                }
+              </Container>
+            </Grid>
+          </Grid>
+        </StyledContainer>
+      </Grid>
+      <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+        {computeRoomContent()}
+      </Grid>
+    </Grid>
+  )
 }
 
 export default RoomContent
--- echo/src/components/rooms/RoomContentChat.js
+++ echo/src/components/rooms/RoomContentChat.js
@@ -6,63 +6,62 @@ import Chat from '../chat/Chat'
 import ChatControls from '../chat/ChatControls'
 import EmojiPicker from "../chat/EmojiPicker";
 
-
 const StyledContainer = styled(Container)(({ theme }) => ({
-    [theme.breakpoints.up('xs')]: {
-        margin: "0 0 0 1rem",
-        height: "calc(100vh - 2rem)",
-        width: "100%",
-        position: "relative",
-        padding: "0",
-        display: "inline-flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
-        maxWidth: "calc(100vw - 20rem)"
-    },
-    [theme.breakpoints.up('lg')]: {
-        margin: "0 0 0 1rem",
-        height: "calc(100vh - 2rem)",
-        width: "100%",
-        position: "relative",
-        padding: "0",
-        display: "inline-flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
-        maxWidth: "calc(100vw - 20rem)"
-    },
-    [theme.breakpoints.up('xl')]: {
-        margin: "0 0 0 1rem",
-        height: "calc(100vh - 2rem)",
-        width: "100%",
-        position: "relative",
-        padding: "0",
-        display: "inline-flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
-        maxWidth: "calc(100vw - 20rem)"
-    },
+  [theme.breakpoints.up('xs')]: {
+    margin: "0 0 0 1rem",
+    height: "calc(100vh - 5rem)",
+    width: "100%",
+    position: "relative",
+    padding: "0",
+    display: "inline-flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    maxWidth: "calc(100vw - 20rem)"
+  },
+  [theme.breakpoints.up('lg')]: {
+    margin: "0 0 0 1rem",
+    height: "calc(100vh - 5rem)",
+    width: "100%",
+    position: "relative",
+    padding: "0",
+    display: "inline-flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    maxWidth: "calc(100vw - 20rem)"
+  },
+  [theme.breakpoints.up('xl')]: {
+    margin: "0 0 0 1rem",
+    height: "calc(100vh - 5rem)",
+    width: "100%",
+    position: "relative",
+    padding: "0",
+    display: "inline-flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    maxWidth: "calc(100vw - 20rem)"
+  },
 }));
 
 function RoomContentChat({ roomId }) {
-    const [emojiPicker, setEmojiPicker] = useState(false);
-    const handleEmojiPicker = () => {
-        // console.log("sto cambianto emojipicker", emojiPicker)
-        setEmojiPicker(!emojiPicker);
-    };
+  const [emojiPicker, setEmojiPicker] = useState(false);
+  const handleEmojiPicker = () => {
+    // console.log("sto cambianto emojipicker", emojiPicker)
+    setEmojiPicker(!emojiPicker);
+  };
 
-    return (
-        <StyledContainer>
-            <Chat currentRoomId={roomId} />
-            <ChatControls onEmojiOn={handleEmojiPicker} roomId={roomId} />
-            <EmojiPicker show={emojiPicker} style={{
-                position: "absolute",
-                bottom: "5rem",
-                right: "1rem",
-                zIndex: "1000",
-            }}
-            />
-        </StyledContainer>
-    )
+  return (
+    <StyledContainer>
+      <Chat currentRoomId={roomId} />
+      <ChatControls onEmojiOn={handleEmojiPicker} roomId={roomId} />
+      <EmojiPicker show={emojiPicker} style={{
+        position: "absolute",
+        bottom: "5rem",
+        right: "1rem",
+        zIndex: "1000",
+      }}
+      />
+    </StyledContainer>
+  )
 }
 
 export default RoomContentChat
