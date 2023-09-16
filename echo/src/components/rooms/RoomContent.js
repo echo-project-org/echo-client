5625a3f570873e6ea3e9e30f8ecb1ca18ef8db1e
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2023-09-16 21:27:01 +0200

    Added switch between screen share and chat

--- echo/src/components/rooms/RoomContent.js
+++ echo/src/components/rooms/RoomContent.js
@@ -2,20 +2,23 @@ import "../../css/chat.css";
 
 import { useState, useEffect } from 'react'
 import RoomContentChat from "./RoomContentChat";
+import RoomContentScreenShares from "./RoomContentScreenShares";
 
 
 
 
 function RoomContent({ roomId }) {
   const [hasUsersStreaming, setHasUsersStreaming] = useState(false);
-  const [contentSelected, setContentSelected] = useState("chat");
+  const [contentSelected, setContentSelected] = useState("screen");
 
-  return (
-    contentSelected === "chat" ?
-      <RoomContentChat roomId={roomId} />
-      :
-      <></>
-  )
+  switch (contentSelected) {
+    case "chat":
+      return <RoomContentChat roomId={roomId} />
+    case "screen":
+      return <RoomContentScreenShares roomId={roomId} />
+    default:
+      return <RoomContentChat roomId={roomId} />
+  }
 }
 
 export default RoomContent
