39a79dbb3fb6613c6c23828df51299423a1a1ba9
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2023-09-16 20:25:04 +0200

    Moved chat to separate component

--- echo/src/components/rooms/RoomContent.js
+++ echo/src/components/rooms/RoomContent.js
@@ -1,69 +1,16 @@
 import "../../css/chat.css";
 
 import { useState, useEffect } from 'react'
-import { Container, styled } from '@mui/material'
+import RoomContentChat from "./RoomContentChat";
 
-import Chat from '../chat/Chat'
-import ChatControls from '../chat/ChatControls'
-import EmojiPicker from "../chat/EmojiPicker";
 
-const StyledContainer = styled(Container)(({ theme }) => ({
-  [theme.breakpoints.up('xs')]: {
-    margin: "0 0 0 1rem",
-    height: "calc(100vh - 2rem)",
-    width: "100%",
-    position: "relative",
-    padding: "0",
-    display: "inline-flex",
-    flexDirection: "column",
-    justifyContent: "space-between",
-    maxWidth: "calc(100vw - 20rem)"
-  },
-  [theme.breakpoints.up('lg')]: {
-    margin: "0 0 0 1rem",
-    height: "calc(100vh - 2rem)",
-    width: "100%",
-    position: "relative",
-    padding: "0",
-    display: "inline-flex",
-    flexDirection: "column",
-    justifyContent: "space-between",
-    maxWidth: "calc(100vw - 20rem)"
-  },
-  [theme.breakpoints.up('xl')]: {
-    margin: "0 0 0 1rem",
-    height: "calc(100vh - 2rem)",
-    width: "100%",
-    position: "relative",
-    padding: "0",
-    display: "inline-flex",
-    flexDirection: "column",
-    justifyContent: "space-between",
-    maxWidth: "calc(100vw - 20rem)"
-  },
-}));
 
 
 function RoomContent({ roomId }) {
-  const [emojiPicker, setEmojiPicker] = useState(false);
-
-  const handleEmojiPicker = () => {
-    // console.log("sto cambianto emojipicker", emojiPicker)
-    setEmojiPicker(!emojiPicker);
-  };
+  const [hasUsersStreaming, setHasUsersStreaming] = useState(false);
 
   return (
-    <StyledContainer>
-      <Chat currentRoomId={roomId}/>
-      <ChatControls onEmojiOn={handleEmojiPicker} roomId={roomId} />
-      <EmojiPicker show={emojiPicker} style={{
-          position: "absolute",
-          bottom: "5rem",
-          right: "1rem",
-          zIndex: "1000",
-        }}
-      />
-    </StyledContainer>
+    RoomContentChat({ roomId })
   )
 }
 
