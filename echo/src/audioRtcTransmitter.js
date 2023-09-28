ba1f10882d574682f37cdf04b0434dde04e0beff
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2023-09-28 11:44:52 +0200

    Added analyser and created input stream

--- echo/src/audioRtcTransmitter.js
+++ echo/src/audioRtcTransmitter.js
@@ -12,6 +12,37 @@ class audioRtcTransmitter {
     this.device = null;
     this.sendTransport = null;
     this.producer = null;
+    this.outChannelCount = 2;
+
+    this.outStream = null;
+    /**
+     * @type {GainNode}
+     * @description Output gain node
+     */
+    this.outGainNode = null;
+
+    /**
+     * @type {GainNode}
+     * @description Voice activity detection gain node
+     */
+    this.vadNode = null;
+
+    this.analyser = null;
+
+    this.constraints = {
+      audio: {
+        channelCount: 2,
+        sampleRate: 48000,
+        sampleSize: 16,
+        volume: 1.0,
+        echoCancellation: false,
+        noiseSuppression: false,
+        autoGainControl: false,
+        deviceId: this.deviceId,
+        googNoiseSupression: false,
+      },
+      video: false,
+    }
   }
 
   createSendTransport() {
@@ -55,6 +86,65 @@ class audioRtcTransmitter {
         errback(err);
       }
     });
+  }
+
+  async startAudioBroadcast() {
+    this.outStream = await navigator.mediaDevices.getUserMedia(this.constraints, err => { console.error(err); return; });
+    const context = new AudioContext();
+
+    const src = context.createMediaStreamSource(this.outStream);
+    const dst = context.createMediaStreamDestination();
+    this.outChannelCount = src.channelCount;
+
+    this.outGainNode = context.createGain();
+    this.vadNode = context.createGain();
+    this.channelSplitter = context.createChannelSplitter(this.outChannelCount);
+
+    src.connect(this.channelSplitter);
+    this.outGainNode.connect(this.channelSplitter);
+    this.outGainNode.connect(this.vadNode);
+    this.vadNode.connect(dst);
+
+    this.analyser = this.createAudioAnalyser(context, this.channelSplitter, this.outChannelCount);
+
+    this.setOutVolume(this.volume);
+    //Add tracks to mediasoup
+  }
+
+  createAudioAnalyser(context, splitter, channelCount) {
+    const analyser = {};
+    const freqs = {};
+    for (let i = 0; i < channelCount; i++) {
+      analyser[i] = context.createAnalyser();
+
+      // for human voice
+      // https://github.com/Jam3/voice-activity-detection/blob/master/index.js
+
+      analyser[i].fftSize = 1024;
+      analyser[i].bufferLen = 1024;
+      analyser[i].smoothingTimeConstant = 0.2;
+      analyser[i].minCaptureFreq = 85;
+      analyser[i].maxCaptureFreq = 255;
+      analyser[i].noiseCaptureDuration = 1000;
+      analyser[i].minNoiseLevel = 0.3;
+      analyser[i].maxNoiseLevel = 0.7;
+      analyser[i].avgNoiseMultiplier = 1.2;
+
+      // analyser[i].minDecibels = -100;
+      // analyser[i].maxDecibels = 0;
+      freqs[i] = new Uint8Array(analyser[i].frequencyBinCount);
+      splitter.connect(analyser[i], i, 0);
+    }
+
+    return {
+      analyser: analyser,
+      freqs: freqs,
+    }
+  }
+
+  setOutVolume(volume) {
+    this.volume = volume;
+    this.outGainNode.gain.value = volume;
   }
 
   /**
