77a7921e53130d217c136c729a59354ec96c7bea
zThundy <antonioantonelli7@gmail.com>
2023-09-29 19:03:11 +0200

    complete restyle of people in room screen

--- echo/src/components/rooms/RoomContentScreenShares.js
+++ echo/src/components/rooms/RoomContentScreenShares.js
@@ -1,84 +1,95 @@
+import "../../css/screenshare.css";
+
 import React from 'react'
-import ReactPlayer from 'react-player';
 import { useState, useEffect } from 'react'
-import { Container, styled } from '@mui/material'
+import { Avatar, Container, Grid, Typography, styled } from '@mui/material'
 import ScreenShareControlIcons from './ScreenShareControlIcons';
 
 import { ep } from '../..';
 
-const StyledContainer = styled(Container)(({ theme }) => ({
+const StyledAvatar = styled(Avatar)(({ theme }) => ({
   [theme.breakpoints.up('xs')]: {
-    margin: "0 0 0 1rem",
-    height: "calc(100vh - 5rem)",
-    width: "100%",
-    position: "relative",
-    padding: "0",
-    display: "inline-flex",
-    flexDirection: "column",
-    justifyContent: "space-between",
-    maxWidth: "calc(100vw - 20rem)"
-  },
-  [theme.breakpoints.up('lg')]: {
-    margin: "0 0 0 1rem",
-    height: "calc(100vh - 5rem)",
-    width: "100%",
-    position: "relative",
-    padding: "0",
-    display: "inline-flex",
-    flexDirection: "column",
-    justifyContent: "space-between",
-    maxWidth: "calc(100vw - 20rem)"
-  },
-  [theme.breakpoints.up('xl')]: {
-    margin: "0 0 0 1rem",
-    height: "calc(100vh - 5rem)",
-    width: "100%",
-    position: "relative",
-    padding: "0",
-    display: "inline-flex",
-    flexDirection: "column",
-    justifyContent: "space-between",
-    maxWidth: "calc(100vw - 20rem)"
+    // put image in the center of parent div
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: "5rem",
+    height: "5rem",
   },
 }));
 
-const screenShareContainerStyle = {
-  display: 'block',
-  position: 'relative',
-  width: '100%',
-  top: "50%",
-  transform: "translateY(-50%)"
-}
+const StyledTypography = styled(Typography)(({ theme }) => ({
+  [theme.breakpoints.up('xs')]: {
+    position: "absolute",
+    top: "15%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    color: theme.palette.text.main,
+    opacity: "0.3",
+  },
+}));
 
-const screenShareWrapperStyle = {
-  backgroundColor: 'black',
-  height: '100%',
-}
+const StyledContainer = styled(Container)(({ theme }) => ({
+  [theme.breakpoints.up('xs')]: {
+    overflow: "auto",
+    display: "flex",
+    flexDirection: "column-reverse",
+    height: "100%",
+    width: "100%",
+    position: "absolute",
+    maxWidth: "calc(100vw - 20rem)",
+    padding: "0",
+  },
+}));
 
 function RoomContentScreenShares({ roomId }) {
-  const [muted, setMuted] = useState(true);
-  // const [screenShareStream, setScreenShareStream] = useState(`http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4`);
-  const [screenShareStream, setScreenShareStream] = useState("");
+  const [users, setUsers] = useState([]);
+  const [focusedUser, setFocusedUser] = useState(null);
+  // const [screenShareStream, setScreenShareStream] = useState("");
 
-  const toggleMuteStream = () => {
-    setMuted(!muted);
-    setScreenShareStream(ep.getVideo(1));
+  useEffect(() => {
+    const users = ep.getUsersInRoom(roomId)
+    console.log("users", users)
+    setUsers(users)
+  }, [roomId])
+
+  const selectUser = (user) => {
+    console.log("selectUser", user);
+    setFocusedUser(user);
+    var myDiv = document.getElementById('screenShareContainer');
+    // myDiv.innerHTML = variableLongText;
+    myDiv.scrollTop = (99999999999999 * -1);
+  }
+  const stopPlayback = () => {
+    setFocusedUser(null);
+  }
+  const computeFocusedUser = () => {
+    if (focusedUser) {
+      return (
+        <Grid item xs={12}>
+          <ScreenShareControlIcons stopPlayback={stopPlayback} />
+        </Grid>
+      )
+    }
   }
 
   return (
-    <StyledContainer>
-      <div style={screenShareWrapperStyle}>
-        <div style={screenShareContainerStyle}>
-          <ReactPlayer
-            url={screenShareStream}
-            playing={true}
-            muted={muted}
-            width="100%"
-            height="100%"
-          />
-          <ScreenShareControlIcons muted={muted} toggleMuteStream={toggleMuteStream} />
-        </div>
-      </div>
+    <StyledContainer id="screenShareContainer">
+      <Grid container gap={3} className="screenshareUserGridContainer noselect">
+        {computeFocusedUser()}
+        {
+          users.map((user) => {
+            return (
+              <Grid item className="screenshareUserContainer" key={user.id} onMouseDown={() => { selectUser(user); }}>
+                <Container className="screenshareUser" sx={{ background: (user.userImage ? `url(${user.userImage})` : "white") }}></Container>
+                <StyledAvatar className="screenshareUserAvatar" src={user.userImage} />
+                <StyledTypography variant="h4">{user.name}</StyledTypography>
+              </Grid>
+            )
+          })
+        }
+      </Grid>
     </StyledContainer>
   )
 }
--- echo/src/components/rooms/ScreenShareControlIcons.js
+++ echo/src/components/rooms/ScreenShareControlIcons.js
@@ -1,62 +1,81 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 import { Grid, Typography } from '@mui/material';
-import { ButtonGroup, Button, Tooltip } from '@mui/material';
+import { ButtonGroup, Button, Tooltip, Container } from '@mui/material';
 import { CancelPresentation, VolumeUp, VolumeOff } from '@mui/icons-material';
+import ReactPlayer from 'react-player';
 
-const ScreenShareControlIcons = ({ muted, toggleMuteStream }) => {
+const ScreenShareControlIcons = ({ stopPlayback }) => {
   const [showControls, setShowControls] = useState(false);
+  const [screenShareStream, setScreenShareStream] = useState(`http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4`);
+  const [muted, setMuted] = useState(true);
 
   const handleMouseEnter = () => {
     setShowControls(true);
   }
-
   const handleMouseLeave = () => {
     setShowControls(false);
   }
-
+  const toggleMuteStream = () => {
+    setMuted(!muted);
+  }
   const stopWaching = () => {
     console.log("Stop watching")
-  }
-
-  const controlsDivStyle = {
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    zIndex: 2,
-    backgroundColor: 'rgba(0,0,0,0.6)',
-    opacity: showControls ? 1 : 0,
-    transition: 'opacity 0.3s ease',
+    stopPlayback();
   }
 
   return (
-    <div style={controlsDivStyle} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-      <Grid container direction='row' alignContent='center' justifyContent='start' style={{ padding: 16 }}>
-        <Grid item>
-          <Typography variant='h5' style={{ color: '#f5e8da' }}>Player</Typography>
+    <Container id="wrapper" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} sx={{
+      margin: "auto",
+      position: "relative",
+      width: "100%",
+      height: "100%",
+    }}>
+      <ReactPlayer
+        url={screenShareStream}
+        playing={true}
+        muted={muted}
+        width="100%"
+        height="100%"
+      />
+      <Container sx={{
+        position: "absolute",
+        top: "0",
+        left: "0",
+        width: "100%",
+        height: "100%",
+        backgroundColor: showControls ? "rgba(0, 0, 0, 0.5)" : "rgba(0, 0, 0, 0.0)",
+        transition: "all 0.1s ease",
+        zIndex: "2",
+      }}>
+        <Grid container direction='row' alignContent='center' justifyContent='start' style={{ padding: 16 }}>
+          <Grid item>
+            <Typography variant='h5' style={{ color: '#f5e8da' }}>Player</Typography>
+          </Grid>
         </Grid>
 
-      </Grid>
-      <Grid container direction='row' alignItems='center' justifyContent='center' paddingBottom={"1rem"}>
-        <ButtonGroup variant='text' className='buttonGroup'>
-          <Tooltip title={"Mute stream"} placement="top" arrow enterDelay={1} enterTouchDelay={20}>
-            <Button disableRipple onClick={toggleMuteStream}>
-              {muted ? <VolumeOff /> : <VolumeUp />}
-            </Button>
-          </Tooltip>
-          <Tooltip title={"Stop watching"} placement="top" arrow enterDelay={1} enterTouchDelay={20}>
-            <Button disableRipple onClick={stopWaching}>
-              <CancelPresentation />
-            </Button>
-          </Tooltip>
-        </ButtonGroup>
-      </Grid>
-    </div>
+        <Grid container direction='row' alignItems='center' justifyContent='center' paddingBottom={"1rem"} zIndex={3} sx={{
+          // align items to bottom of container
+          position: "absolute",
+          bottom: ".1rem",
+          left: "0",
+          width: "100%",
+        }}>
+          <ButtonGroup variant='text'>
+            <Tooltip title={"Mute stream"} placement="top" arrow enterDelay={1} enterTouchDelay={20}>
+              <Button disableRipple onClick={toggleMuteStream}>
+                {muted ? <VolumeOff /> : <VolumeUp />}
+              </Button>
+            </Tooltip>
+            <Tooltip title={"Stop watching"} placement="top" arrow enterDelay={1} enterTouchDelay={20}>
+              <Button disableRipple onClick={stopWaching}>
+                <CancelPresentation />
+              </Button>
+            </Tooltip>
+          </ButtonGroup>
+        </Grid>
+      </Container>
+    </Container>
   )
 }
 
