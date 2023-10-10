13bcaa668f986038c5a5997d445be90707621588
zThundy <antonioantonelli7@gmail.com>
2023-10-10 16:44:45 +0200

    more work on friends list

--- echo/src/components/rooms/RoomContentFriends.js
+++ echo/src/components/rooms/RoomContentFriends.js
@@ -1,3 +1,5 @@
+import "../../css/friends.css";
+
 import { useEffect, useState } from "react";
 
 import { Avatar, Grid, Container, Button } from "@mui/material";
@@ -25,38 +27,233 @@ function RoomContentFriends({ }) {
         name: "John Doe",
         online: "1",
         userImage: "https://via.placeholder.com/110",
+      },
+      {
+        id: "4",
+        name: "John Doe",
+        online: "1",
+        userImage: "https://via.placeholder.com/110",
+      },
+      {
+        id: "4",
+        name: "John Doe",
+        online: "1",
+        userImage: "https://via.placeholder.com/110",
+      },
+      {
+        id: "4",
+        name: "John Doe",
+        online: "1",
+        userImage: "https://via.placeholder.com/110",
+      },
+      {
+        id: "4",
+        name: "John Doe",
+        online: "1",
+        userImage: "https://via.placeholder.com/110",
+      },
+      {
+        id: "4",
+        name: "John Doe",
+        online: "1",
+        userImage: "https://via.placeholder.com/110",
+      },
+      {
+        id: "4",
+        name: "John Doe",
+        online: "1",
+        userImage: "https://via.placeholder.com/110",
+      },
+      {
+        id: "4",
+        name: "John Doe",
+        online: "1",
+        userImage: "https://via.placeholder.com/110",
+      },
+      {
+        id: "4",
+        name: "John Doe",
+        online: "1",
+        userImage: "https://via.placeholder.com/110",
+      },
+      {
+        id: "4",
+        name: "John Doe",
+        online: "1",
+        userImage: "https://via.placeholder.com/110",
+      },
+      {
+        id: "4",
+        name: "John Doe",
+        online: "1",
+        userImage: "https://via.placeholder.com/110",
+      },
+      {
+        id: "4",
+        name: "John Doe",
+        online: "1",
+        userImage: "https://via.placeholder.com/110",
+      },
+      {
+        id: "4",
+        name: "John Doe",
+        online: "1",
+        userImage: "https://via.placeholder.com/110",
+      },
+      {
+        id: "4",
+        name: "John Doe",
+        online: "1",
+        userImage: "https://via.placeholder.com/110",
+      },
+      {
+        id: "4",
+        name: "John Doe",
+        online: "1",
+        userImage: "https://via.placeholder.com/110",
+      },
+      {
+        id: "4",
+        name: "John Doe",
+        online: "1",
+        userImage: "https://via.placeholder.com/110",
+      },
+      {
+        id: "4",
+        name: "John Doe",
+        online: "1",
+        userImage: "https://via.placeholder.com/110",
+      },
+      {
+        id: "4",
+        name: "John Doe",
+        online: "1",
+        userImage: "https://via.placeholder.com/110",
+      },
+      {
+        id: "4",
+        name: "John Doe",
+        online: "1",
+        userImage: "https://via.placeholder.com/110",
+      },
+      {
+        id: "4",
+        name: "John Doe",
+        online: "1",
+        userImage: "https://via.placeholder.com/110",
+      },
+      {
+        id: "4",
+        name: "John Doe",
+        online: "1",
+        userImage: "https://via.placeholder.com/110",
+      },
+      {
+        id: "4",
+        name: "John Doe",
+        online: "1",
+        userImage: "https://via.placeholder.com/110",
+      },
+      {
+        id: "4",
+        name: "John Doe",
+        online: "1",
+        userImage: "https://via.placeholder.com/110",
+      },
+      {
+        id: "4",
+        name: "John Doe",
+        online: "1",
+        userImage: "https://via.placeholder.com/110",
+      },
+      {
+        id: "4",
+        name: "John Doe",
+        online: "1",
+        userImage: "https://via.placeholder.com/110",
+      },
+      {
+        id: "4",
+        name: "John Doe",
+        online: "1",
+        userImage: "https://via.placeholder.com/110",
+      },
+      {
+        id: "4",
+        name: "John Doe",
+        online: "1",
+        userImage: "https://via.placeholder.com/110",
+      },
+      {
+        id: "4",
+        name: "John Doe",
+        online: "1",
+        userImage: "https://via.placeholder.com/110",
+      },
+      {
+        id: "4",
+        name: "John Doe",
+        online: "1",
+        userImage: "https://via.placeholder.com/110",
+      },
+      {
+        id: "4",
+        name: "John Doe",
+        online: "1",
+        userImage: "https://via.placeholder.com/110",
+      },
+      {
+        id: "4",
+        name: "John Doe",
+        online: "1",
+        userImage: "https://via.placeholder.com/110",
+      },
+      {
+        id: "4",
+        name: "John Doe",
+        online: "1",
+        userImage: "https://via.placeholder.com/110",
+      },
+      {
+        id: "4",
+        name: "John Doe",
+        online: "1",
+        userImage: "https://via.placeholder.com/110",
       }
     ])
   }, []);
 
   return (
-    <>
-      {
-        friends.map((user, index) => {
-          console.log(index);
-          return (
-            <Grid container key={index} flexDirection={"column"} display={"flex"}>
-              <Grid item xs={12}>
-                <Avatar alt={user.id} src={user.userImage} />
+    <Container className="friends-list-container">
+      <Container className="friends-list-overflow">
+        {
+          friends.map((user, index) => {
+            console.log(index);
+            return (
+              <Grid container className="friend-container" key={index} flexDirection={"row"} display={"flex"}>
+                <Grid item xs={3}>
+                  <Avatar className="userAvatar" alt={user.id} src={user.userImage} />
+                </Grid>
+                <Grid item xs={3} className="userInfo">
+                  <span className="userName">{user.name}</span>
+                  <span className="userStatus">{user.online}</span>
+                </Grid>
+                <Grid item xs={8}>
+                  <Container className="buttonsContainer">
+                    <Button>
+                      <ChatBubble />
+                    </Button>
+                    <Button>
+                      <Call />
+                    </Button>
+                  </Container>
+                </Grid>
               </Grid>
-              <Grid item xs={12}>
-                {user.name}
-              </Grid>
-              <Grid item xs={12}>
-                <Container className="buttonsContainer">
-                  <Button>
-                    <ChatBubble />
-                  </Button>
-                  <Button>
-                    <Call />
-                  </Button>
-                </Container>
-              </Grid>
-            </Grid>
-          )
-        })
-      }
-    </>
+            )
+          })
+        }
+      </Container>
+    </Container>
   )
 }
 
