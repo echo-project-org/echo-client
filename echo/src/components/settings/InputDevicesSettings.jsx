d23a00e7bf2bad093b6bc67452bb4766302e7f42
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2023-10-18 17:23:04 +0200

    Added vadDetection setting in settings

--- echo/src/components/settings/InputDevicesSettings.jsx
+++ echo/src/components/settings/InputDevicesSettings.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { Stack, Slider, Typography, Grid, ClickAwayListener } from '@mui/material';
-import { Mic, ArrowDropDown, ArrowDropUp, CheckCircle } from '@mui/icons-material';
+import { Mic, ArrowDropDown, ArrowDropUp, CheckCircle, RecordVoiceOver } from '@mui/icons-material';
 
 import { ep, storage } from "../../index";
 
@@ -14,7 +14,7 @@ const CurrentDevice = ({ inputDevices, inputDevice, showList }) => {
     </Typography>
   )
 }
-  
+
 const DevicesSelectList = ({ inputDevices, handleInputDeviceChange, inputDevice, showList }) => {
   inputDevices.forEach(device => {
     const startIndex = device.name.indexOf('(', device.name.indexOf('(') + 1);
@@ -45,13 +45,16 @@ const DevicesSelectList = ({ inputDevices, handleInputDeviceChange, inputDevice,
 function InputDevicesSettings({ inputDevices }) {
   const [inputDevice, setInputDevice] = useState('default');
   const [micVolume, setMicVolulme] = useState(100);
+  const [vadTreshold, setVadTreshold] = useState(0);
   const [showList, setShowList] = useState(false);
 
   useEffect(() => {
     setInputDevice(storage.get('inputAudioDeviceId') || "default");
     ep.setMicrophoneVolume(storage.get('micVolume') || 1);
     ep.setMicrophoneDevice(storage.get('inputAudioDeviceId') || "default");
+    ep.setVadTreshold(storage.get('vadTreshold') || 1);
     setMicVolulme(Math.floor(storage.get('micVolume') * 100) || 100);
+    setVadTreshold(Math.floor(storage.get('vadTreshold') * 100) || 0);
   }, []);
 
   const handleInputDeviceChange = (event) => {
@@ -65,6 +68,13 @@ function InputDevicesSettings({ inputDevices }) {
     storage.set('micVolume', newValue / 100);
     setMicVolulme(newValue);
     ep.setMicrophoneVolume(newValue / 100);
+  };
+
+  const handleVadTresholdChange = (event, newValue) => {
+    //set user volume
+    storage.set('vadTreshold', newValue / 100);
+    setVadTreshold(newValue);
+    ep.setVadTreshold(newValue / 100);
   };
 
   const deviceListToggle = (status = true) => {
@@ -96,6 +106,20 @@ function InputDevicesSettings({ inputDevices }) {
             aria-label="Volume"
             value={micVolume}
             onChange={handleMicVolumeChange}
+            size='medium'
+          />
+        </Stack>
+      </div>
+      <div style={{ paddingRight: "2%", width: "95%" }}>
+        <Stack spacing={2} direction="row" alignItems="center">
+          <RecordVoiceOver fontSize="medium" />
+          <Slider
+            sx={{ width: "95%" }}
+            valueLabelDisplay="auto"
+            valueLabelFormat={(v) => { return v + "%" }}
+            aria-label="Volume"
+            value={vadTreshold}
+            onChange={handleVadTresholdChange}
             size='medium'
           />
         </Stack>
