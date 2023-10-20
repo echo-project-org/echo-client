d7871e4ace53ed93e47a2c8c532bdf6a95c41f31
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2023-10-20 09:12:53 +0200

    Added add/remove friend button switch

--- echo/src/components/user/OnlineUsersMenuItems.jsx
+++ echo/src/components/user/OnlineUsersMenuItems.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-
+import { useState, useEffect } from 'react'
 import { MenuItem } from '@mui/material'
-import { Message, DoDisturb, Gavel, Settings, PersonAdd } from '@mui/icons-material'
+import { Message, DoDisturb, Gavel, Settings, PersonAdd, PersonRemove } from '@mui/icons-material'
 import ScreenShareIcon from '@mui/icons-material/ScreenShare';
 import { ep, storage } from "../../index";
 
 function OnlineUsersMenuItems({ user, broadcastingVideo, handleClose }) {
+    const [isFriend, setIsFriend] = useState(false);
+
     const startWatchingBroadcast = () => {
         ep.startReceivingVideo(user.id);
     }
@@ -15,7 +17,12 @@ function OnlineUsersMenuItems({ user, broadcastingVideo, handleClose }) {
             <>
                 {broadcastingVideo ? <MenuItem onClick={startWatchingBroadcast}><ScreenShareIcon fontSize="10px" style={{ marginRight: ".3rem" }} />Watch broadcast</MenuItem> : null}
                 <MenuItem onClick={handleClose}><Message fontSize="10px" style={{ marginRight: ".3rem" }} />Send message</MenuItem>
-                <MenuItem onClick={handleClose}><PersonAdd fontSize="10px" style={{ marginRight: ".3rem" }} /> Ad as friend</MenuItem>
+                {
+                    isFriend ?
+                        <MenuItem onClick={handleClose}><PersonRemove fontSize="10px" style={{ marginRight: ".3rem", color: "red" }} />Remove friend</MenuItem>
+                        :
+                        <MenuItem onClick={handleClose}><PersonAdd fontSize="10px" style={{ marginRight: ".3rem" }} /> Ad friend</MenuItem>
+                }
                 <MenuItem onClick={handleClose}><DoDisturb fontSize="10px" style={{ marginRight: ".3rem", color: "red" }} />Kick</MenuItem>
                 <MenuItem onClick={handleClose}><Gavel fontSize="10px" style={{ marginRight: ".3rem", color: "red" }} /> Ban</MenuItem>
             </>
