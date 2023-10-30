3b5ee4a64567ae77eb8555f1e1a3f21921da3507
zThundy <antonioantonelli7@gmail.com>
2023-10-30 11:01:11 +0100

    still bored, more website

--- echo-website/src/components/body/Routes/Status.jsx
+++ echo-website/src/components/body/Routes/Status.jsx
@@ -1,9 +1,30 @@
+import { Container, styled } from "@mui/material"
+
+const StyledBodyContainer = styled(Container)(({ theme }) => ({
+    [theme.breakpoints.up('xs')]: {
+        textAlign: "center",
+        color: "white",
+        display: "flex",
+        height: "50%",
+        justifyContent: "center",
+        flexDirection: "column",
+        lineHeight: "3.5rem",
+        "a": {
+            color: "#bc5dc8",
+            textDecoration: "none",
+            "&:hover": {
+                color: "#ea94f4",
+            },
+        },
+    }
+}))
 
 function Status({ }) {
     return (
-        <>
-            <h1>Status</h1>
-        </>
+        <StyledBodyContainer>
+            <h1>Nothing to see here!</h1>
+            <p>For now...</p>
+        </StyledBodyContainer>
     )
 }
 
--- echo-website/src/components/footer/Footer.jsx
+++ echo-website/src/components/footer/Footer.jsx
@@ -1,6 +1,28 @@
-import { Container } from "@mui/material"
+import { Container, Typography, styled } from "@mui/material"
+import { Favorite } from '@mui/icons-material';
+
+const StyledFooterTypography = styled(Typography)(({ theme }) => ({
+    [theme.breakpoints.up('xs')]: {
+        margin: "auto 0 auto 0",
+        color: "white",
+        display: "flex",
+        padding: "0 0 0 1rem",
+        "a": {
+            color: "#bc5dc8",
+            padding: "0 .5rem 0 .5rem",
+            textDecoration: "none",
+            "&:hover": {
+                color: "#ea94f4",
+            },
+        },
+    }
+}))
 
 function Footer({ }) {
+    const openNewPage = (username) => {
+        window.open(`https://github.com/${username}`, "_blank");
+    }
+
     return (
         <Container sx={{
             position: "fixed",
@@ -10,7 +32,11 @@ function Footer({ }) {
             height: "1.8rem",
             boxShadow: "0px 0px 5px 0px rgba(255,255,255,0.4)",
             bottom: "0",
+            display: "flex",
         }}>
+            <StyledFooterTypography>
+                Made with <Favorite sx={{ margin: "0rem .5rem 0 .5rem", color: "red" }} /> by <a onClick={() => openNewPage("zthundy")} href="#">zThundy</a> & <a onClick={() => openNewPage("kurygabriele")} href="#">Kury</a>
+            </StyledFooterTypography>
             
         </Container>
     )
