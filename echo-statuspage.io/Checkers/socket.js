1817abbff428f1e7d15ccf42b4f6d407d63a4f0c
zThundy <antonioantonelli7@gmail.com>
2023-11-07 00:31:02 +0100

    same as before

--- echo-statuspage.io/Checkers/socket.js
+++ echo-statuspage.io/Checkers/socket.js
@@ -4,13 +4,13 @@ const socketIO = require("socket.io-client");
 class SocketCheck {
     constructor(service) {
         this.service = service;
-        if (service.fireOnStart) this.check();
-        this.interval = setInterval(this.check.bind(this), this.service.interval * 1000);
-        
         this.socket = socketIO(this.service.url, {
             path: "/socket.io",
             query: { id: "status" }
         });
+        
+        if (service.fireOnStart) this.check();
+        this.interval = setInterval(this.check.bind(this), this.service.interval * 1000);
     }
 
     check() {
