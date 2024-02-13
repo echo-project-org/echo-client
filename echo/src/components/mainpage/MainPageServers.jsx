b3111eeb67893ff91f9ca2a50d5aa962c27a8d11
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2024-02-13 13:24:55 +0100

    Updated server browser api call to add multi server support
    Servers are now fetched from api
    When a user selects a server, that server will be joined
    Moved sigle server component so standalone

--- echo/src/components/mainpage/MainPageServers.jsx
+++ echo/src/components/mainpage/MainPageServers.jsx
@@ -1,55 +1,33 @@
 import { useState, useEffect } from 'react';
-import { Grid, Slide } from '@mui/material';
 import { useNavigate } from "react-router-dom";
 import { TransitionGroup } from 'react-transition-group';
 
 import { ep, storage } from "../../index";
-import StylingComponents from '../../StylingComponents';
+import MainPageServersComponent from './MainPageServersComponent';
 
 const api = require('../../api');
 
 function MainPageServers({ }) {
-  const [servers, setFriends] = useState([]);
+  const [servers, setServers] = useState([]);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    // api.call('servers', "GET")
-    //   .then((res) => {
-    //     setFriends(res.data);
-    //   })
-    //   .catch((err) => {
-    //     console.error(err.message);
-    //   });
-    setFriends([
-      {
-        name: "Server name",
-        logo: "https://echo.kuricki.com/api/users/image/2",
-        description: "This is a description of the Server",
-      },
-      {
-        name: "Server name",
-        logo: "https://echo.kuricki.com/api/users/image/3",
-        description: "This is a description of the Server",
-      },
-      {
-        name: "Server name",
-        logo: "https://echo.kuricki.com/api/users/image/5",
-        description: "This is a description of the Server",
-      },
-      {
-        name: "Server name",
-        logo: "https://echo.kuricki.com/api/users/image/1",
-        description: "This is a description of the Server",
-      },
-      {
-        name: "Server name",
-        logo: "https://echo.kuricki.com/api/users/image/4",
-        description: "This is a description of the Server",
-      },
-    ])
-  }, []);
+  const updateServers = () => {
+    api.call('servers/')
+      .then((res) => {
+        setServers(res.json);
+      })
+      .catch((err) => {
+        console.error(err.message);
+      });
+  
+  }
 
-  const enterServer = async () => {
+  useEffect(() => {
+    updateServers();
+  }, []);
+  
+
+  const enterServer = async (serverId) => {
     // TODO: check the initial status of user (maybe get it from the login form?)
     // and check if we need to update it or not
     api.call('users/status', "POST", { id: storage.get('id'), status: "1" })
@@ -74,17 +52,7 @@ function MainPageServers({ }) {
     <TransitionGroup>
       {servers.map((Server, id) => {
         return (
-          <Slide key={id} direction="right" in={true} mountOnEnter unmountOnExit timeout={id * 100}>
-            <StylingComponents.MainPage.StyledMainPageGrid container flexDirection={"row"} onClick={enterServer}>
-              <Grid item>
-                <StylingComponents.MainPage.StyledMainPageAvatar src={Server.logo} />
-              </Grid>
-              <Grid item>
-                <h3>{Server.name}</h3>
-                <p>{Server.description}</p>
-              </Grid>
-            </StylingComponents.MainPage.StyledMainPageGrid>
-          </Slide>
+          MainPageServersComponent({ Server, id, enterServer })
         )
       })}
     </TransitionGroup>
