ac13a82dfe0d12279d3929b97a5751ae8db3c537
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2024-04-01 21:01:39 +0200

    Started work on keyboard shortcuts

--- echo/craco.config.js
+++ echo/craco.config.js
@@ -6,6 +6,7 @@ module.exports = {
             '@root': path.resolve(__dirname, './src/'),
             '@views': path.resolve(__dirname, './src/views'),
             '@lib': path.resolve(__dirname, './src/lib'),
+            '@img': path.resolve(__dirname, './src/img'),
             '@components': path.resolve(__dirname, './src/components'),
             '@css': path.resolve(__dirname, './src/css'),
             '@audio': path.resolve(__dirname, './src/audio'),
