4f400650fb3232027c6aef4a8a7338813652cade
Kuricki Gabriele <gabrijele39@gmail.com>
2024-04-27 12:55:28 +0200

    Added check to make transparency work only in win11
    The electron function only works under win 11 22H2 and up

--- echo/src/lib/hexToRgba.js
+++ echo/src/lib/hexToRgba.js
@@ -2,8 +2,14 @@ const hexToRgba = (hex, alpha) => {
     const r = parseInt(hex.slice(1, 3), 16);
     const g = parseInt(hex.slice(3, 5), 16);
     const b = parseInt(hex.slice(5, 7), 16);
-    
-    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+
+    if (process.platform === "win32" && process.getSystemVersion().startsWith("11")){
+        //return transparent colors
+        return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+    } else {
+        //if not windows 11 return solid colors
+        return `rgba(${r}, ${g}, ${b}, 1)`;
+    }
 }
 
 export default hexToRgba;
