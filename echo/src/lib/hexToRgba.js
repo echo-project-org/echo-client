febab60b6123a578f8491f445cb5b367bd438d7b
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2024-04-27 13:07:43 +0200

    Fixed windows version detection

--- echo/src/lib/hexToRgba.js
+++ echo/src/lib/hexToRgba.js
@@ -1,13 +1,14 @@
+const os = window.require('os');
+
 const hexToRgba = (hex, alpha) => {
     const r = parseInt(hex.slice(1, 3), 16);
     const g = parseInt(hex.slice(3, 5), 16);
     const b = parseInt(hex.slice(5, 7), 16);
 
-    if (process.platform === "win32" && process.getSystemVersion().startsWith("11")){
-        //return transparent colors
-        return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+    if(os.platform() === 'win32'  && os.release().startsWith('10.0.22')){
+        //if windows 11
+        return `rgb(${r}, ${g}, ${b}, ${alpha})`;
     } else {
-        //if not windows 11 return solid colors
         return `rgba(${r}, ${g}, ${b}, 1)`;
     }
 }
