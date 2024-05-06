c0d9493e1fd1580a68ce4abe0c5199e270c49574
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2024-05-06 16:13:12 +0200

    Started work on microphone capturer

---
+++ echo/src/lib/mediasoup/MicrophoneCapturer.js
@@ -0,0 +1,128 @@
+import { createAudioConstraints } from '@lib/mediasoup/constraints.js';
+import { AudioAnalyser } from '@lib/mediasoup/Audioanalyser.js';
+const { warn, error, info } = require("@lib/logger");
+
+class MicrophoneCapturer {
+    constructor(inputDeviceId, volume, muted = false, talkingThreshold = 0.3, noiseSuppression = false, echoCancellation = false, autoGainControl = false) {
+        this.muted = muted;
+        this.volume = volume;
+
+        this.inputDeviceId = inputDeviceId;
+        this.talkingThreshold = talkingThreshold;
+        this.noiseSuppression = noiseSuppression;
+        this.echoCancellation = echoCancellation;
+        this.autoGainControl = autoGainControl;
+        this.channelCount = 2;
+
+        this.audioContext = null;
+        this.outGainNode = null;
+        this.vadNode = null;
+        this.channelSplitter = null;
+        this.analyser = null;
+        this.stream = null;
+    }
+
+    /**
+     * Starts caputring audio from the microphone.
+     * @param {string} inputDeviceId 
+     * @returns {Promise<MediaStreamTrack>} Audio track for the microphone.
+     */
+    async start(inputDeviceId = 'default') {
+        return new Promise(async (resolve, reject) => {
+            try {
+                this.stream = await navigator.mediaDevices.getUserMedia(
+                    createAudioConstraints(
+                        this.echoCancellation,
+                        this.noiseSuppression,
+                        this.autoGainControl,
+                        inputDeviceId
+                    ));
+
+                this.audioContext = new AudioContext();
+                const src = this.audioContext.createMediaStreamSource(this.stream);
+                const dst = this.audioContext.createMediaStreamDestination();
+                this.channelCount = src.channelCount;
+
+                this.outGainNode = this.audioContext.createGain();
+                this.vadNode = this.audioContext.createGain();
+                this.channelSplitter = this.audioContext.createChannelSplitter(this.channelCount);
+
+                src.connect(this.outGainNode);
+                this.outGainNode.connect(this.channelSplitter);
+                this.outGainNode.connect(this.vadNode);
+                this.vadNode.connect(dst);
+
+                this.analyser = new AudioAnalyser(this.audioContext, this.channelSplitter, this.channelCount, this.talkingThreshold);
+                this.analyser.start((v) => {
+                    if(v) {
+                        this.vadNode.gain.value = 1.0;
+                        //TODO emit talking event
+                    } else {
+                        this.vadNode.gain.value = 0.0;
+                        //TODO emit silence event
+                    }
+                })
+
+
+                this.setVolume(this.volume);
+                if (this.muted) {
+                    this.mute();
+                }
+
+                resolve(dst.stream.getAudioTracks()[0]);
+            } catch (err) {
+                reject(err);
+            }
+        });
+    }
+
+    stop() {
+        if (this.stream) {
+            this.stream.getTracks().forEach((track) => {
+                track.stop();
+            });
+            this.stream = null;
+        }
+        
+        this.audioContext.close();
+        this.analyser.stop();
+    }
+
+    setVolume(volume) {
+        if (volume >= 0 && volume <= 1) {
+            this.volume = volume;
+            this.outGainNode.gain.value = this.volume;
+        } else {
+            warn("Volume must be between 0 and 1")
+        }
+    }
+
+    mute() {
+        this.muted = true;
+        if (this.stream) {
+            this.stream.getAudioTracks().forEach((track) => {
+                track.enabled = false;
+            });
+        }
+    }
+
+    unmute() {
+        this.muted = false;
+        if (this.stream) {
+            this.stream.getAudioTracks().forEach((track) => {
+                track.enabled = true;
+            });
+        }
+    }
+
+    setTalkingThreshold(threshold) {
+        if (threshold >= 0 && threshold <= 1) {
+            this.talkingThreshold = threshold;
+            this.analyser.setTalkingThreshold(this.talkingThreshold);
+        } else {
+            warn("Talking threshold must be between 0 and 1")
+        }
+    }
+}
+
+export default MicrophoneCapturer;
