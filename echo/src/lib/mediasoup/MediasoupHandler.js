58401a567998c3f8122ad720e00439674fc3394d
Kuricki Gabriele <gabrijele39@gmail.com>
2024-05-06 12:24:16 +0200

    Added createTransport method

--- echo/src/lib/mediasoup/MediasoupHandler.js
+++ echo/src/lib/mediasoup/MediasoupHandler.js
@@ -4,7 +4,7 @@ const mediasoup = require('mediasoup-client');
 const { warn, error, log } = require('@lib/logger');
 
 class MediasoupHandler {
-    constructor(id, inputDeviceId = 'default', outputDeviceId = 'default', micVolume = 1.0, speakerVolume = 1.0, noiseSuppression = false, echoCancellation = false, autoGainControl = false){
+    constructor(id, inputDeviceId = 'default', outputDeviceId = 'default', micVolume = 1.0, speakerVolume = 1.0, noiseSuppression = false, echoCancellation = false, autoGainControl = false) {
         this.id = id;
 
         this.inputDeviceId = inputDeviceId;
@@ -20,6 +20,72 @@ class MediasoupHandler {
 
         this.mediasoupDevice = null;
         this.transports = new Map();
+    }
+
+    /**
+ * Creates a new receive transport for mediasoup based on the given data.
+ * @async
+ * @param {string} type - The type of transport to create.
+ * @param {Object} data - The data required to create the receive transport.
+ * @param {string} data.id - The ID of the receive transport.
+ * @param {RTCIceParameters} data.iceParameters - The ICE parameters for the transport.
+ * @param {RTCIceCandidate[]} data.iceCandidates - The ICE candidates for the transport.
+ * @param {RTCDtlsParameters} data.dtlsParameters - The DTLS parameters for the transport.
+ * @param {RTCSctpParameters} data.sctpParameters - The SCTP parameters for the transport.
+ * @param {RTCIceServer[]} data.iceServers - The ICE servers for the transport.
+ * @param {string} data.iceTransportPolicy - The ICE transport policy for the transport.
+ * @param {Object} data.additionalSettings - Additional settings for the transport.
+ */
+    async createTransport(type, data) {
+        return new Promise(async (resolve, reject) => {
+            if (!this.mediasoupDevice) {
+                reject('mediasoupDevice not initialized');
+                return;
+            }
+
+            if (!type) {
+                reject('type is required');
+                return;
+            }
+
+            if (!data) {
+                reject('data is required');
+                return;
+            }
+
+            if (!this.mediasoupDevice.loaded) {
+                // Load the device with the given rtpCapabilities
+                await this.mediasoupDevice.load({ routerRtpCapabilities: data.rtpCapabilities });
+            }
+
+            let transport;
+            if (type === 'audioOut' || type === 'videoOut') {
+                transport = await this.mediasoupDevice.createSendTransport(data);
+            } else if (type === 'audioIn' || type === 'videoIn') {
+                transport = await this.mediasoupDevice.createRecvTransport(data);
+            } else {
+                reject('Invalid transport type');
+            }
+            
+            transport.on('connect', async ({ dtlsParameters }, callback, errback) => {
+                // TODO Send the DTLS parameters to the server
+            });
+
+            if(type === 'audioOut') {
+                // use audio out transport to check connection state
+                transport.on('connectionstatechange', (state) => {
+                    // TODO Handle the connection state change
+                });
+            }
+
+            if(type === 'audioOut' || type === 'videoOut') {
+                transport.on("produce", async ({ kind, rtpParameters, appData }, callback, errback) => {
+                    // TODO Send the producer data to the server
+                });
+            }
+            this.transports.set(type, transport);
+            resolve(transport);
+        });
     }
 }
 
