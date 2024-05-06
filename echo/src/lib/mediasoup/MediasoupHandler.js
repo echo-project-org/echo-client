a137aee2f7edf253a23826d59c9bc7d5d811a418
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2024-05-06 16:15:44 +0200

    Removed unused attributs from msh

--- echo/src/lib/mediasoup/MediasoupHandler.js
+++ echo/src/lib/mediasoup/MediasoupHandler.js
@@ -4,19 +4,11 @@ const mediasoup = require('mediasoup-client');
 const { warn, error, log } = require('@lib/logger');
 
 class MediasoupHandler {
-    constructor(id, inputDeviceId = 'default', outputDeviceId = 'default', micVolume = 1.0, speakerVolume = 1.0, noiseSuppression = false, echoCancellation = false, autoGainControl = false) {
+    constructor(id, inputDeviceId = 'default', outputDeviceId = 'default',) {
         this.id = id;
 
         this.inputDeviceId = inputDeviceId;
         this.outputDeviceId = outputDeviceId;
-
-        this.micVolume = micVolume;
-        this.speakerVolume = speakerVolume;
-
-        this.noiseSuppression = noiseSuppression;
-        this.echoCancellation = echoCancellation;
-        this.autoGainControl = autoGainControl;
-        this.outChannelCount = 2;
 
         this.mediasoupDevice = null;
         this.transports = new Map();
